cd764cba226bdae6e4c756f8cb6413e5aadfcf8a
afoteas <andreasfoteas@hotmail.com>
2023-09-06 23:52:48 +0300

    Time: 222 ms (5.23%), Space: 16.2 MB (71.05%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.py
+++ 0009-palindrome-number/0009-palindrome-number.py
@@ -1,6 +1,20 @@
+import math
 class Solution:
     def isPalindromeString(self, x: int) -> bool:
         return str(x) == str(x)[::-1]
 
     def isPalindrome(self, x: int) -> bool:
-        return str(x) == str(x)[::-1]     
+        if x < 0 :
+            return False
+        elif x <10:
+            return True    
+        else:
+            l = int(math.log10(x))+1
+            print(f'l={l}')
+            for i in range(int(l/2)):
+                print(f'i={i}')
+                print(f'x // pow(10,l-i-1) / pow(10,i)={x // pow(10,l-i-1) / pow(10,i)}')
+                print(f'(x % pow(10,i+1)) // pow(10,i)={(x % pow(10,i+1)) // pow(10,i)}')
+                if (x // pow(10,l-i-1) % 10) != ((x % pow(10,i+1)) // pow(10,i)):
+                    return False
+            return True         
