f1ab127357f9bbb5ae527161052c17bfcbc8a92b
afoteas <andreasfoteas@hotmail.com>
2023-10-18 01:10:08 +0300

    Time: 144 ms (54.11%), Space: 19.3 MB (55.06%) - LeetHub

--- 0135-candy/0135-candy.py
+++ 0135-candy/0135-candy.py
@@ -55,7 +55,8 @@ class Solution:
         
         for i in range(n-1):
             if R[i] < R[i+1]:
-                ans[i+1] = max(1 + ans[i], ans[i+1])
+                # ans[i+1] = max(1 + ans[i], ans[i+1])
+                ans[i+1] = 1 + ans[i]
                 
         for i in range(n-2, -1, -1):
             if R[i+1] < R[i]:
