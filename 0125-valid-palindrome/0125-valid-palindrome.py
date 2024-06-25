9b5d6adc9b6c4200f192891d7e73205e8ee73a20
afoteas <andreasfoteas@hotmail.com>
2024-06-25 11:22:42 +0300

    Time: 44 ms (61.78%), Space: 17.9 MB (23.04%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.py
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -1,11 +1,11 @@
 import re
 class Solution:
     def isPalindrome(self, s: str) -> bool:
-        ts = re.sub(r'[^a-z0-9]', '', s.strip().lower())
+        s = re.sub(r'[^a-z0-9]', '', s.strip().lower())
         p = 0
-        pi = len(ts)
+        pi = len(s)
         for i in range(0,int(pi/2)):
-            if ts[p+i] != ts[pi-i-1]:
+            if s[p+i] != s[pi-i-1]:
                 return False
         return True
                 
