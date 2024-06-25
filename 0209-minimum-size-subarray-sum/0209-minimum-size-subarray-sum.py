78d5867e6150f77f913e1f774592f316b1c76324
afoteas <andreasfoteas@hotmail.com>
2024-06-25 19:40:28 +0300

    Time: 204 ms (21.47%), Space: 30.3 MB (23.65%) - LeetHub

---
+++ 0209-minimum-size-subarray-sum/0209-minimum-size-subarray-sum.py
@@ -0,0 +1,41 @@
+class Solution:
+    def minSubArrayLenSlow(self, target: int, nums: List[int]) -> int:
+        window = []
+        count = 100000
+        solution = False
+        for num in nums:
+            window.append(num)
+            if solution:
+                window.pop(0)
+            while sum(window) >= target:
+                solution = True
+                print("solution")
+                if len(window) < count:
+                    count = len(window)
+                window.pop(0)
+        if solution:
+            return count
+        else:
+            return 0
+
+    def minSubArrayLen(self, target: int, nums: List[int]) -> int:
+        l=0
+        r=0
+        s=nums[0]
+        count = 1000000
+        ln=len(nums)
+        while r<ln:
+            if s>=target:
+                count=min(count,r-l+1)
+                s-=nums[l]
+                l+=1
+            else:
+                r+=1
+                s+=nums[min(r,ln-1)]
+        if count<1000000:
+            return count
+        else:
+            return 0
+
+
+        
