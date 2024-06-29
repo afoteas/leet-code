8a66b09f666a2580a42e4c4aa86c864274a3a30a
afoteas <andreasfoteas@hotmail.com>
2024-06-29 13:13:34 +0300

    Time: 207 ms (6.03%), Space: 17.9 MB (75.05%) - LeetHub

--- 0224-basic-calculator/0224-basic-calculator.py
+++ 0224-basic-calculator/0224-basic-calculator.py
@@ -1,5 +1,5 @@
 class Solution:
-    def calculate(self, s: str) -> int:
+    def calculateSlow(self, s: str) -> int:
         par=["+"]
         papar="+"
         numbers=[]
@@ -39,7 +39,47 @@ class Solution:
             else:
                 numbers.append(-int(cur_num))
         return sum(numbers)
-                
+
+    def calculate(self, s: str) -> int:
+        par=["+"]
+        papar="+"
+        total_sum=0
+        cur_num=""
+        sign="+"
+        ss=s.replace(" ", "")
+        for c in ss:
+            if c in ["0","1","2","3","4","5","6","7","8","9"]:
+                cur_num+=c
+            else:
+                if cur_num:
+                    pros = par.count("-")
+                    if pros%2 == 0:
+                        papar="+"
+                    else:
+                        papar="-"
+                    if papar == sign:
+                        total_sum += int(cur_num)
+                    else:
+                        total_sum -= int(cur_num)
+                    cur_num = ""
+                if c == "(":
+                    par.append(sign)
+                    sign="+"
+                elif c == ")":
+                    par.pop()
+                else:
+                    sign = c
+        if cur_num:
+            pros = par.count("-")
+            if pros%2 == 0:
+                papar="+"
+            else:
+                papar="-"
+            if sign == papar:
+                total_sum += int(cur_num)
+            else:
+                total_sum -= int(cur_num)
+        return total_sum                
                  
                 
 
