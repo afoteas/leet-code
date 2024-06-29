5308027f82d5c982d4d01e5accf78e2ebf39e255
afoteas <andreasfoteas@hotmail.com>
2024-06-29 16:50:50 +0300

    Time: 741 ms (5.01%), Space: 69.1 MB (10.41%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.py
+++ 0053-maximum-subarray/0053-maximum-subarray.py
@@ -15,7 +15,7 @@ class Solution:
             return max(nums[i] + solve(i+1, True), 0 if must_pick else solve(i+1, False))
         return solve(0, False)
 
-    def maxSubArray(self, nums: List[int]) -> int:
+    def maxSubArrayDPtab(self, nums: List[int]) -> int:
         dp = [[0]*len(nums) for i in range(2)]
         dp[0][0], dp[1][0] = nums[0], nums[0]
         for i in range(1, len(nums)):
@@ -23,8 +23,18 @@ class Solution:
             dp[0][i] = max(dp[0][i-1], dp[1][i])
         return dp[0][-1]
 
-    def maxSubArray(self, nums: List[int]) -> int:
+    def maxSubArrayTabBetter(self, nums: List[int]) -> int:
         dp = [*nums]
         for i in range(1, len(nums)):
             dp[i] = max(nums[i], nums[i] + dp[i-1])
         return max(dp)
+
+    def maxSubArray(self, nums):
+        pre, suf = [*nums], [*nums]
+        for i in range(1, len(nums)):       pre[i] += max(0, pre[i-1])
+        for i in range(len(nums)-2,-1,-1):  suf[i] += max(0, suf[i+1])
+        def maxSubArray(A, L, R):
+            if L == R: return A[L]
+            mid = (L + R) // 2
+            return max(maxSubArray(A, L, mid), maxSubArray(A, mid+1, R), pre[mid] + suf[mid+1])
+        return maxSubArray(nums, 0, len(nums)-1)
