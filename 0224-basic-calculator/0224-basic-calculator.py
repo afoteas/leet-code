ed4d9b1f4a20cf0815f6a54e0993876c58e6ea51
afoteas <andreasfoteas@hotmail.com>
2024-06-29 14:18:11 +0300

    Time: 78 ms (40.57%), Space: 17.8 MB (90.14%) - LeetHub

--- 0224-basic-calculator/0224-basic-calculator.py
+++ 0224-basic-calculator/0224-basic-calculator.py
@@ -41,44 +41,34 @@ class Solution:
         return sum(numbers)
 
     def calculate(self, s: str) -> int:
-        par=["+"]
-        papar="+"
+        total=1
+        neg_count=[1]
         total_sum=0
         cur_num=""
-        sign="+"
+        current=1
         ss=s.replace(" ", "")
         for c in ss:
             if c in ["0","1","2","3","4","5","6","7","8","9"]:
                 cur_num+=c
             else:
                 if cur_num:
-                    pros = par.count("-")
-                    if pros%2 == 0:
-                        papar="+"
-                    else:
-                        papar="-"
-                    if papar == sign:
-                        total_sum += int(cur_num)
-                    else:
-                        total_sum -= int(cur_num)
+                    total_sum += total*current*int(cur_num)
+                    current=1
                     cur_num = ""
-                if c == "(":
-                    par.append(sign)
-                    sign="+"
-                elif c == ")":
-                    par.pop()
+                if c in ["(",")"]:
+                    if c == "(":
+                        neg_count.append(current)
+                    else:
+                        neg_count.pop()
+                    total=(-1)**neg_count.count(-1)
+                    current=1
                 else:
-                    sign = c
+                    if c=="+":
+                        current=1
+                    else:
+                        current=-1
         if cur_num:
-            pros = par.count("-")
-            if pros%2 == 0:
-                papar="+"
-            else:
-                papar="-"
-            if sign == papar:
-                total_sum += int(cur_num)
-            else:
-                total_sum -= int(cur_num)
+            total_sum += total*current*int(cur_num)
         return total_sum                
                  
                 
