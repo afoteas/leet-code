9023d173ec2098f93c2b9846a21145dc0801ae2f
afoteas <andreasfoteas@hotmail.com>
2024-06-30 16:42:03 +0300

    Time: 43 ms (11.81%), Space: 16.7 MB (8.62%) - LeetHub

--- 0198-house-robber/0198-house-robber.py
+++ 0198-house-robber/0198-house-robber.py
@@ -7,11 +7,19 @@ class Solution:
             dp=[dp[1],dp[2],max(dp[0],dp[1])+nums[i]]
         return max(dp)
 
-    def rob(self, nums: List[int]) -> int:
+    def rob2(self, nums: List[int]) -> int:
         if len(nums) < 3:
             return max(nums)
         nums[2]+=nums[0]
         for i in range(3,len(nums)):
             nums[i] = max(nums[i-2],nums[i-3]) + nums[i]
         return max(nums[-2:])
+        
+    def rob(self, nums: List[int]) -> int:
+        @cache
+        def rec(i:int) -> int:
+            if i < 0:
+                return 0
+            return max(rec(i-2) + nums[i], rec(i-1))
+        return rec(len(nums)-1)
         
