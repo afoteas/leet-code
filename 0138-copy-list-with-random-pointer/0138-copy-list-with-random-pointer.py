6cedaf5fe5012141db9a614f891d1c94d23ce186
afoteas <andreasfoteas@hotmail.com>
2024-07-03 17:46:52 +0300

    Time: 39 ms (50.35%), Space: 17.3 MB (91.49%) - LeetHub

--- 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.py
+++ 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.py
@@ -12,4 +12,46 @@ class Solution:
     def copyRandomList(self, head: 'Optional[Node]') -> 'Optional[Node]':
         dc = copy.deepcopy(head)
         return dc
+
+
+    def copyRandomList(self, head: 'Optional[Node]') -> 'Optional[Node]':
+
+        if not head:
+            return None
+        
+        curr = head
+
+        # add mode nodes betwwen the old ones
+        while curr:
+            nn=Node(curr.val)
+            nn.next = curr.next
+            curr.next = nn
+            curr = nn.next
+        
+        curr = head
+
+        while curr:
+            if curr.random:
+                curr.next.random = curr.random.next
+            if curr.next.next:
+                curr = curr.next.next
+            else:
+                break
+        
+        curr = head
+        new_head = head.next
+        new_curr = new_head
+
+        while new_curr:
+            curr.next = new_curr.next
+            if curr.next:
+                new_curr.next = curr.next.next
+            else:
+                new_curr.next= None
+            new_curr = new_curr.next
+        
+        return new_head
+
+
+        
         
