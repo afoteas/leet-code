67ebe5f60b2008f8bbcb79777c501a5bfb8550c2
afoteas <andreasfoteas@hotmail.com>
2024-07-04 04:46:29 +0300

    Time: 906 ms (5.14%), Space: 17.2 MB (26.49%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -1,8 +1,8 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
         tl = list(t)
-        if len(s) != len(t):
-            return False
+        # if len(s) != len(t):
+        #     return False
         for c in s:
             if c in tl:
                 tl.remove(c)
@@ -10,4 +10,6 @@ class Solution:
                 return False
         if len(tl) == 0:
             return True
+        else:
+            return False
         
