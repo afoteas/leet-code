50a4fc60fbc36ecd0f93a904dfbfd54aee820463
afoteas <andreasfoteas@hotmail.com>
2024-07-04 04:56:13 +0300

    Time: 53 ms (36.44%), Space: 16.7 MB (95.16%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -13,5 +13,20 @@ class Solution:
         else:
             return False
 
-    def isAnagram(self, s: str, t: str) -> bool:
+    def isAnagram2(self, s: str, t: str) -> bool:
         return sorted(t) == sorted(s)
+    
+    def isAnagram(self, s: str, t: str) -> bool:
+        sd = {}
+        td = {}
+        for c in s:
+            if c in sd:
+                sd[c]+=1
+            else:
+                sd[c]=0
+        for c in t:
+            if c in td:
+                td[c]+=1
+            else:
+                td[c]=0
+        return td == sd
