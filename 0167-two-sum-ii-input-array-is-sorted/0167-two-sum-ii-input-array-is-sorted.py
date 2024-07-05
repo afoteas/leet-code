8cce8766bdfaa521a8fd21962eb18ba9a9749cdd
afoteas <andreasfoteas@hotmail.com>
2024-07-05 20:48:31 +0300

    Time: 8941 ms (5.01%), Space: 17.9 MB (12.25%) - LeetHub

---
+++ 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.py
@@ -0,0 +1,29 @@
+class Solution:
+    def twoSumSlow(self, numbers: List[int], target: int) -> List[int]:
+        for i in range(len(numbers)-1):
+            for j in range(i+1, len(numbers)):
+                if numbers[i]+numbers[j]==target:
+                    return [i+1,j+1]
+
+    def twoSum(self, numbers: List[int], target: int) -> List[int]:
+        test=[]
+        for number in numbers:
+            test.append(number-target)
+        ret = []
+        for number in numbers:
+            try:
+                index = test.index(-number)
+                print(ret)
+                if (index+1) in ret:
+                    return [index+1,index+2]
+                else:
+                    ret.append(index + 1)
+            except ValueError:
+                continue
+        if len(ret) > 2:
+            for i in range(len(ret)):
+                 for j in range(i+1, len(ret)):
+                    if numbers[ret[i]-1]+numbers[ret[j]-1]==target and ret[j] != ret[i]:
+                        return [ret[j],ret[i]]
+        print("peos")
+        return ret[::-1]
