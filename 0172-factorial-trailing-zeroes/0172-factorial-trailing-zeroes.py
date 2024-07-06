5ec666ab50d7cc1060c8be17c240ad09b7c05377
afoteas <andreasfoteas@hotmail.com>
2024-07-06 03:40:41 +0300

    Time: 171 ms (26.17%), Space: 36.6 MB (5.23%) - LeetHub

--- 0172-factorial-trailing-zeroes/0172-factorial-trailing-zeroes.py
+++ 0172-factorial-trailing-zeroes/0172-factorial-trailing-zeroes.py
@@ -11,9 +11,7 @@ class Solution:
     def trailingZeroes(self, n: int) -> int:
         if n==0:
             return 0
-        kr2 = 0
         kr5 = 0
-        zeros = 0
         for i in range(1, n+1):
             ans = i
             while ans%5 == 0:
