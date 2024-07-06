a01d0778da0dc7f1a4ff9aa57ec24983db135e80
afoteas <andreasfoteas@hotmail.com>
2024-07-06 03:38:47 +0300

    Time: 325 ms (22.56%), Space: 36.2 MB (5.23%) - LeetHub

--- 0172-factorial-trailing-zeroes/0172-factorial-trailing-zeroes.py
+++ 0172-factorial-trailing-zeroes/0172-factorial-trailing-zeroes.py
@@ -1,6 +1,6 @@
 import numpy as np
 class Solution:
-    def trailingZeroes(self, n: int) -> int:
+    def trailingZeroesBest(self, n: int) -> int:
         out = 0
         while n:
             temp = n//5
@@ -8,7 +8,7 @@ class Solution:
             n = temp
         return out
     
-    def trailingZeroesMine(self, n: int) -> int:
+    def trailingZeroes(self, n: int) -> int:
         if n==0:
             return 0
         kr2 = 0
@@ -16,9 +16,9 @@ class Solution:
         zeros = 0
         for i in range(1, n+1):
             ans = i
-            while ans%10 == 0:
-                zeros += 1
-                ans = ans//10
+            # while ans%10 == 0:
+            #     zeros += 1
+            #     ans = ans//10
             while ans%5 == 0:
                 kr5 += 1
                 ans = ans//5
