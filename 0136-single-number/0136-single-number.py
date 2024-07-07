c8161179717321e6f5b6839538ec5cc2e25f7212
afoteas <andreasfoteas@hotmail.com>
2024-07-08 00:14:28 +0300

    Time: 796 ms (11.74%), Space: 19.4 MB (9.8%) - LeetHub

--- 0136-single-number/0136-single-number.py
+++ 0136-single-number/0136-single-number.py
@@ -11,13 +11,11 @@ class Solution:
         return 0
 
     def singleNumber(self, nums: List[int]) -> int:
-        mySet = set(nums)
-        for n in mySet:
-            nums.remove(n)
-        # print(nums)
-        # print(mySet)
-        for n in mySet:
-            if not n in nums:
-                return n
-        return 0
+        values = []
+        for n in nums:
+            if n in values:
+                values.remove(n)
+            else:
+                values.append(n)
+        return values[0]
         
