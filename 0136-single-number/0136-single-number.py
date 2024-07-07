1fda552d594e2fb475faccadadc105823bca6608
afoteas <andreasfoteas@hotmail.com>
2024-07-08 00:10:50 +0300

    Time: 894 ms (11.57%), Space: 19.4 MB (7.82%) - LeetHub

---
+++ 0136-single-number/0136-single-number.py
@@ -0,0 +1,13 @@
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        mySet = set(nums)
+        for n in mySet:
+            nums.remove(n)
+        print(nums)
+        print(mySet)
+        for n in mySet:
+            if not n in nums:
+                return n
+        return 0
+
+        
