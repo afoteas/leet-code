c324d8e4b4d36bc01b975262dcd111f52b6297be
afoteas <andreasfoteas@hotmail.com>
2024-07-10 21:24:37 +0300

    Time: 1075 ms (73.16%), Space: 16.8 MB (91.74%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.py
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.py
@@ -5,7 +5,9 @@ class Solution:
         for i in range(n):
             for j in range(i):
                 if nums[i] > nums[j] and dp[i] < dp[j] + 1:
-                    dp[i] = dp[j] + 1
+                    # print(f"inside if, dp[i]={dp[i]},dp[j]={dp[j]}")
+                    dp[i] +=1
+            # print(f"i={i}, dp={dp}")
         return max(dp)
 
 
