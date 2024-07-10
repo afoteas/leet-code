f8cdcbe3b98691110030f857e996e8f05d95b0a4
afoteas <andreasfoteas@hotmail.com>
2024-07-10 22:29:12 +0300

    Time: 58 ms (96.82%), Space: 17 MB (29.55%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.py
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.py
@@ -1,14 +1,23 @@
 class Solution:
-    def lengthOfLIS(self, nums: List[int]) -> int:
+    def lengthOfLISON(self, nums: List[int]) -> int:
         n = len(nums)
         dp = [1] * n
         for i in range(n):
             for j in range(i):
                 if nums[i] > nums[j] and dp[i] < dp[j] + 1:
-                    # print(f"inside if, dp[i]={dp[i]},dp[j]={dp[j]}")
-                    dp[i] +=1
-            # print(f"i={i}, dp={dp}")
+                    print(f"inside if, dp[i]={dp[i]},dp[j]={dp[j]}")
+                    dp[i] = dp[j] + 1
+            print(f"i={i}, dp={dp}")
         return max(dp)
 
 
+    def lengthOfLIS(self, nums: List[int]) -> int:
+        sub = []
+        for x in nums:
+            if len(sub) == 0 or sub[-1] < x:
+                sub.append(x)
+            else:
+                idx = bisect_left(sub, x)  # Find the index of the first element >= x
+                sub[idx] = x  # Replace that number with x
+        return len(sub)
         
