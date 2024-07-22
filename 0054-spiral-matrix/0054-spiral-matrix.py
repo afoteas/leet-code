70e6959bc9c9130737e5e45ee4abd05ff4a3e771
afoteas <andreasfoteas@hotmail.com>
2024-07-23 02:25:29 +0300

    Time: 33 ms (73.8%), Space: 16.8 MB (8.97%) - LeetHub

--- 0054-spiral-matrix/0054-spiral-matrix.py
+++ 0054-spiral-matrix/0054-spiral-matrix.py
@@ -8,38 +8,38 @@ class Solution:
         for d in range(max([depth_v,depth_h])):
             if d < depth_v and d < depth_h:
                 i = d
-                print("one")
+                # print("one")
                 for j in range(d, size_h-d):
                     retList.append(matrix[i][j])
                     print(matrix[i][j])
                 j = size_h - d  - 1
-                print("two")
+                # print("two")
                 for i in range(d+1, size_v-d-1):
                     retList.append(matrix[i][j])
                     print(matrix[i][j])
                 i = size_v-d-1
-                print("three")
+                # print("three")
                 for j in range(size_h-d-1,d-1,-1):
                     retList.append(matrix[i][j])
-                    print(matrix[i][j])
+                    # print(matrix[i][j])
                 j = d
-                print(f"four, d={d}")
+                # print(f"four, d={d}")
                 for i in range(size_v-d-2,d, -1):
                     retList.append(matrix[i][j])
                     print(matrix[i][j])
             elif d == depth_v and not size_v == 2:
                 i = d
-                print("extra1")
+                # print("extra1")
                 for j in range(d, size_h-d):
                     retList.append(matrix[i][j])
                     print(matrix[i][j])
                 break
             elif d >= depth_h and not size_h == 2:
-                print(f"extra2, size_v={size_v}, size_h={size_h}")
+                # print(f"extra2, size_v={size_v}, size_h={size_h}")
                 j = size_h - d  - 1
                 for i in range(d, size_v-d):
                     retList.append(matrix[i][j])
-                    print(matrix[i][j])
+                    # print(matrix[i][j])
                 break
         if size_v == size_h and size_v%2:
             retList.append(matrix[depth_v][depth_v])
