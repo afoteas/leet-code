6fb1bd2b49696763e901ff25a0bf7679ab9c6ecb
afoteas <andreasfoteas@hotmail.com>
2024-07-24 23:31:25 +0300

    Time: 50 ms (25.17%), Space: 19.4 MB (48.2%) - LeetHub

--- 0230-kth-smallest-element-in-a-bst/0230-kth-smallest-element-in-a-bst.py
+++ 0230-kth-smallest-element-in-a-bst/0230-kth-smallest-element-in-a-bst.py
@@ -9,17 +9,13 @@ class Solution:
         self.m=0
         self.ret=0
         def mSmall(root):
-            print("entered")
             if root.left:
-                print("root.left")
                 mSmall(root.left)
             if self.m == k:
-                print("m == k")
                 return
             self.m+=1
             self.ret = root.val
             if root.right:
-                print("root.right")
                 mSmall(root.right)
         mSmall(root)
         return self.ret
