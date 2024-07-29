9e3570c87b058bf29fc340d757d50a49926a4ba0
afoteas <andreasfoteas@hotmail.com>
2024-07-29 20:05:34 +0300

    Time: 43 ms (91.17%), Space: 16.7 MB (26.01%) - LeetHub

--- 0383-ransom-note/0383-ransom-note.py
+++ 0383-ransom-note/0383-ransom-note.py
@@ -9,6 +9,14 @@ class Solution:
         return True
 
     def canConstruct(self, ransomNote: str, magazine: str) -> bool:
+        for l in ransomNote:
+            if l in magazine:
+                magazine = magazine.replace(l,"",1)
+            else:
+                return False
+        return True
+
+    def canConstructFast(self, ransomNote: str, magazine: str) -> bool:
         if len(ransomNote) > len(magazine):
             return False
         for i in set(ransomNote):
