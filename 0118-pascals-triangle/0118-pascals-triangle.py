6be9af0b0e6cc9b7bdf52b777283e5e8feff7676
afoteas <andreasfoteas@hotmail.com>
2024-07-30 19:40:08 +0300

    Time: 32 ms (83.33%), Space: 16.6 MB (25.7%) - LeetHub

--- 0118-pascals-triangle/0118-pascals-triangle.py
+++ 0118-pascals-triangle/0118-pascals-triangle.py
@@ -2,8 +2,6 @@ class Solution:
     def generate(self, numRows: int) -> List[List[int]]:
         if numRows == 1:
             return [[1]]
-        elif numRows == 2:
-            return [[1],[1,1]]
         else:
             ret = [[1]]
             for i in range(3,numRows+2):
