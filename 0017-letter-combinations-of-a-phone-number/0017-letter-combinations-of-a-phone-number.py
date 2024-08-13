760fb09e414c05dcd698bf8a766199ee99fe1ebb
afoteas <andreasfoteas@hotmail.com>
2024-08-13 14:58:59 +0300

    Time: 31 ms (81.35%), Space: 16.6 MB (38.98%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.py
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.py
@@ -5,13 +5,9 @@ class Solution:
         digitList=["abc","def","ghi","jkl","mno","pqrs","tuv","wxyz"]
         ret = list(digitList[int(digits[0])-2])
         for digit in digits[1:]:
-            print("here1")
             retret = []
             for char in digitList[int(digit)-2]:
-                print("here2")
-                
                 for re in ret:
-                    print(f"here3, re= {re}")
                     retret.append(f"{re}{char}")
             ret = retret
         return ret
