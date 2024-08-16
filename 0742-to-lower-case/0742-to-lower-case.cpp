1a6b61d9853a1a511bb0fd3f147b8252e1341435
afoteas <andreasfoteas@hotmail.com>
2024-08-16 17:53:55 +0300

    Time: 3 ms (42.3%), Space: 7.7 MB (40.82%) - LeetHub

--- 0742-to-lower-case/0742-to-lower-case.cpp
+++ 0742-to-lower-case/0742-to-lower-case.cpp
@@ -10,13 +10,3 @@ public:
         return s;
     }
 };
-
-
-    // def toLowerCase(self, s: str) -> str:
-    //     ret = ""
-    //     for c in s:
-    //         if ord(c) < 91 and ord(c)>64:
-    //             ret+=chr(ord(c)+32)
-    //         else:
-    //             ret+=c
-    //     return ret
