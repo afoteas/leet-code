b383ed583100618f2c7d490e0c3e173b004d21fc
afoteas <andreasfoteas@hotmail.com>
2024-08-16 16:23:42 +0300

    Time: 117 ms (40.06%), Space: 99.4 MB (16.43%) - LeetHub

---
+++ 0932-monotonic-array/0932-monotonic-array.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    bool isMonotonic(vector<int>& nums) {
+        int previous = nums[0];
+        int monotonic = 0; // 0 unitialized, 1 increasing, 2 decreasing
+        for (int num: nums) {
+            if (num == previous) continue;
+            if (monotonic) {
+                if (num < previous && monotonic != 2) {
+                    return false;
+                } else if (num > previous && monotonic !=1) {
+                    return false;
+                }
+            } else {
+                monotonic = (num > previous)? 1 : 2;
+            }
+            previous = num;
+        }
+        return true;
+        
+    }
+};
