bd79df8e482e549fb3b7ed2828a6d57450e7afba
afoteas <andreasfoteas@hotmail.com>
2024-08-17 13:37:47 +0300

    Time: 1511 ms (75.12%), Space: 50.9 MB (49.76%) - LeetHub

---
+++ 2067-maximum-number-of-points-with-cost/2067-maximum-number-of-points-with-cost.py
@@ -0,0 +1,30 @@
+import operator
+# index, value = max(enumerate(my_list), key=operator.itemgetter(1))
+class Solution:
+    def maxPoints(self, points: List[List[int]]) -> int:
+        rows, cols = len(points), len(points[0])
+        for i in range(1,rows):
+            right = [0]*cols
+            right[-1] = points[i-1][-1]
+            for j in range(cols-2,-1,-1):
+                right[j]=max(right[j+1]-1, points[i-1][j])
+            left = points[i-1][0]
+            points[i][0] = max(left,right[0]) + points[i][0]
+            for j in range(1, cols):
+                left = max(left-1, points[i-1][j])
+                points[i][j]=max(left,right[j])+points[i][j]
+        return max(points[-1])
+
+
+
+
+
+
+
+    # def climbStairsDP(self, n: int) -> int:
+    #     if n in [1, 2]:
+    #         return n
+    #     dp = {1:1,2:2}
+    #     for i in range(3,n+1):
+    #         dp[i]=dp[i-1] + dp[i-2]
+    #     return dp[n]
