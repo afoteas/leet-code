d45b6ecaea3009435dc6d794b98740d61271a498
afoteas <andreasfoteas@hotmail.com>
2024-08-17 19:41:26 +0300

    Time: 198 ms (55.59%), Space: 119 MB (43.87%) - LeetHub

--- 2067-maximum-number-of-points-with-cost/2067-maximum-number-of-points-with-cost.cpp
+++ 2067-maximum-number-of-points-with-cost/2067-maximum-number-of-points-with-cost.cpp
@@ -25,20 +25,6 @@ public:
         return *std::max_element(points.back().begin(), points.back().end());
     }
 
-//     def maxPoints(self, points: List[List[int]]) -> int:
-//         rows, cols = len(points), len(points[0])
-//         for i in range(1,rows):
-//             right = [0]*cols
-//             right[-1] = points[i-1][-1]
-//             for j in range(cols-2,-1,-1):
-//                 right[j]=max(right[j+1]-1, points[i-1][j])
-//             left = points[i-1][0]
-//             points[i][0] = max(left,right[0]) + points[i][0]
-//             for j in range(1, cols):
-//                 left = max(left-1, points[i-1][j])
-//                 points[i][j]=max(left,right[j])+points[i][j]
-//         return max(points[-1])
-
     long long maxPoints(vector<vector<int>>& points) {
         int rows = points.size();
         int cols = points[0].size();
