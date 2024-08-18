d07f173e5d258e7bc1225b80d2330705e91356bb
afoteas <andreasfoteas@hotmail.com>
2024-08-18 18:35:25 +0300

    Time: 4 ms (60.74%), Space: 7.9 MB (98.13%) - LeetHub

--- 0264-ugly-number-ii/0264-ugly-number-ii.cpp
+++ 0264-ugly-number-ii/0264-ugly-number-ii.cpp
@@ -17,15 +17,15 @@ public:
                 min(next_multiple_of_3, next_multiple_of_5));
             ugly[i] = next_ugly_no;
             if (next_ugly_no == next_multiple_of_2) {
-                i2 = i2 + 1;
+                ++i2;
                 next_multiple_of_2 = ugly[i2] * 2;
             }
             if (next_ugly_no == next_multiple_of_3) {
-                i3 = i3 + 1;
+                ++i3;
                 next_multiple_of_3 = ugly[i3] * 3;
             }
             if (next_ugly_no == next_multiple_of_5) {
-                i5 = i5 + 1;
+                ++i5;
                 next_multiple_of_5 = ugly[i5] * 5;
             }
         }  
