9c11001e0274c4dbd5f64fb882d170a8b0a5f0b0
afoteas <andreasfoteas@hotmail.com>
2024-08-18 19:03:31 +0300

    Time: 64 ms (34.35%), Space: 31.7 MB (26.89%) - LeetHub

--- 0264-ugly-number-ii/0264-ugly-number-ii.cpp
+++ 0264-ugly-number-ii/0264-ugly-number-ii.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
 
-    int nthUglyNumber(int n) {
+    int nthUglyNumberDp(int n) {
         unsigned ugly[n]; 
         unsigned i2 = 0, i3 = 0, i5 = 0;
         unsigned next_multiple_of_2 = 2;
@@ -27,4 +27,36 @@ public:
         }  
         return next_ugly_no;
     }
+
+    int nthUglyNumber(int n)
+    {
+        // Base cases...
+        if (n == 1 or n == 2 or n == 3 or n == 4 or n == 5)
+            return n;
+    
+        set<long long int> s;
+        s.insert(1);
+        n--;
+    
+        while (n) {
+            auto it = s.begin();
+    
+            // Get the beginning element of the set
+            long long int x = *it;
+    
+            // Deleting the ith element
+            s.erase(it);
+    
+            // Inserting all the other options
+            s.insert(x * 2);
+            s.insert(x * 3);
+            s.insert(x * 5);
+            n--;
+        }
+    
+        // The top of the set represents the nth ugly number
+        return *s.begin();
+    }
+ 
+
 };
