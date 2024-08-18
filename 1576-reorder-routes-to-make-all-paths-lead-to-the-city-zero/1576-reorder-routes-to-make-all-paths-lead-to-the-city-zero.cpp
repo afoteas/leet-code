f2d80c784f61b832775c3b57297edc648c2e8a50
afoteas <andreasfoteas@hotmail.com>
2024-08-18 22:20:37 +0300

    Time: 212 ms (95.75%), Space: 104.7 MB (86.44%) - LeetHub

--- 1576-reorder-routes-to-make-all-paths-lead-to-the-city-zero/1576-reorder-routes-to-make-all-paths-lead-to-the-city-zero.cpp
+++ 1576-reorder-routes-to-make-all-paths-lead-to-the-city-zero/1576-reorder-routes-to-make-all-paths-lead-to-the-city-zero.cpp
@@ -27,7 +27,7 @@ public:
         return s;
     }
 
-    int minReorder(int n, vector<vector<int>>& connections) {
+    int minReorderBfs(int n, vector<vector<int>>& connections) {
         int s = 0;
         
         // Create map mapping vertices to their adjacent edges
@@ -60,6 +60,26 @@ public:
         }
         
         return s;
+    }
+
+    int count = 0;
+    void dfs(int node, int parent, vector<vector<pair<int, int>>>& adj) {
+        for (auto& [neighbor, sign] : adj[node]) {
+            if (neighbor != parent) {
+                count += sign;
+                dfs(neighbor, node, adj);
+            }
+        }
+    }
+
+    int minReorder(int n, vector<vector<int>>& connections) {
+        vector<vector<pair<int, int>>> adj(n);
+        for (auto& connection : connections) {
+            adj[connection[0]].push_back({connection[1], 1});
+            adj[connection[1]].push_back({connection[0], 0});
+        }
+        dfs(0, -1, adj);
+        return count;
     }
 };
 
