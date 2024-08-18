83de8ee0089a5823ea0d42dd182e4826482b4470
afoteas <andreasfoteas@hotmail.com>
2024-08-18 21:49:05 +0300

    Time: 877 ms (90.35%), Space: 42.2 MB (93.63%) - LeetHub

--- 1576-reorder-routes-to-make-all-paths-lead-to-the-city-zero/1576-reorder-routes-to-make-all-paths-lead-to-the-city-zero.py
+++ 1576-reorder-routes-to-make-all-paths-lead-to-the-city-zero/1576-reorder-routes-to-make-all-paths-lead-to-the-city-zero.py
@@ -1,5 +1,5 @@
 class Solution:
-    def minReorder(self, n: int, connections: List[List[int]]) -> int:
+    def minReorderInitial(self, n: int, connections: List[List[int]]) -> int:
         s = 0
         # create dictionary mapping vertices to their adjacent edges
         adj = defaultdict(list)
@@ -23,4 +23,30 @@ class Solution:
             curr = new
             seen.update(new)
 
-        return s          
+        return s
+
+    def minReorder(self, n: int, connections: List[List[int]]) -> int:
+        s = 0
+        # create dictionary mapping vertices to their adjacent edges
+        adj = defaultdict(list)
+        for c in connections:
+            adj[c[0]].append(c)
+            adj[c[1]].append(c)
+
+        # run the search
+        curr = {0}
+        seen = set()
+        while curr:
+            next_curr = set()
+            for v in curr:
+                for e in adj[v]:
+                    if e[0] not in seen and e[1] == v:
+                        # pointing the 'right' direction
+                        next_curr.add(e[0])
+                    elif e[0] == v and e[1] not in seen:
+                        # pointing the 'wrong' direction
+                        next_curr.add(e[1])
+                        s += 1
+                seen.add(v)
+            curr = next_curr
+        return s     
