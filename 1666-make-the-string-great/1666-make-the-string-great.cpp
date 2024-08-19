5aca53d944eb4455eeb792744aa0d16bec75b92e
afoteas <andreasfoteas@hotmail.com>
2024-08-19 21:53:00 +0300

    Time: 0 ms (100%), Space: 8 MB (50.69%) - LeetHub

--- 1666-make-the-string-great/1666-make-the-string-great.cpp
+++ 1666-make-the-string-great/1666-make-the-string-great.cpp
@@ -6,28 +6,13 @@ public:
         if (l < 2) {
             return s;
         }
-        for(int i=0; i<l;++i) {
-            if(ret.length() > 0 && abs(int(s.at(i) - int(ret.back()))) == 32) {
+        for(const auto& ch: s) {
+            if(ret.length() > 0 && abs(int(ch) - int(ret.back())) == 32) {
                 ret.pop_back();
             } else {
-                ret.push_back(s.at(i));
+                ret.push_back(ch);
             }
         }
         return ret;    
     }
 };
-
-
-        // if len(s) < 2:
-        //     return s
-        // ret = []
-        // i = 0
-        // last = "?"
-        // l = len(s)
-        // while i < l:
-        //     if ret and abs(ord(s[i]) - ord(ret[-1])) == 32:
-        //         ret.pop()
-        //     else:
-        //         ret.append(s[i])
-        //     i+=1
-        // return "".join(ret)
