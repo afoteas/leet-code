ea5f76bd094f736ec992309e49b728746fbfb50d
afoteas <andreasfoteas@hotmail.com>
2024-08-19 21:36:22 +0300

    Time: 42 ms (28.23%), Space: 16.4 MB (67.34%) - LeetHub

---
+++ 1666-make-the-string-great/1666-make-the-string-great.py
@@ -0,0 +1,18 @@
+class Solution:
+    def makeGood(self, s: str) -> str:
+
+        if len(s) < 2:
+            return s
+        ret = []
+
+        i = 0
+        last = "?"
+        while i < len(s):
+            if not ret:
+                ret.append(s[i])
+            elif abs(ord(s[i]) - ord(ret[-1])) == 32:
+                ret.pop()
+            else:
+                ret.append(s[i])
+            i+=1
+        return "".join(ret)
