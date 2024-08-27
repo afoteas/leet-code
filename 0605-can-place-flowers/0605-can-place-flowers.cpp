558fb66102366f9c2850b43954ba4621a7d370f6
afoteas <andreasfoteas@hotmail.com>
2024-08-27 16:58:26 +0300

    Time: 7 ms (92.43%), Space: 23.1 MB (46.62%) - LeetHub

--- 0605-can-place-flowers/0605-can-place-flowers.cpp
+++ 0605-can-place-flowers/0605-can-place-flowers.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    bool canPlaceFlowers(vector<int>& flowerbed, int n) {
+    bool canPlaceFlowersMine(vector<int>& flowerbed, int n) {
         int fsize = flowerbed.size();
         if (n == 0) {
             return true;
@@ -32,5 +32,18 @@ public:
             return true;
         }
         return n == 0;
+    }
+
+    bool canPlaceFlowers(vector<int>& flowerbed, int n) {
+        for (int i = 0; i < flowerbed.size(); i++) {
+            bool left = i == 0 || flowerbed[i - 1] == 0;
+            bool right = i == flowerbed.size() - 1 || flowerbed[i + 1] == 0;
+            
+            if (left && right && flowerbed[i] == 0) {
+                flowerbed[i] = 1;
+                n--;
+            }
+        }
+        return n <= 0;        
     }
 };
