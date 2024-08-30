df5663938ee0cd9b3ac61d24876dbdb6eb143341
afoteas <andreasfoteas@hotmail.com>
2024-08-31 02:01:28 +0300

    Time: 24 ms (13.72%), Space: 18 MB (24.14%) - LeetHub

---
+++ 0813-all-paths-from-source-to-target/0813-all-paths-from-source-to-target.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    vector<vector<int>> allPathsSourceTarget(vector<vector<int>>& graph) {
+        int t = graph.size()-1;
+        vector<vector<int>> ret;
+        function<void(int, vector<int>)> dfs = [&](int u, vector<int> path) {
+            cout << u <<'\n' ;  
+            if(u==t) {
+                ret.push_back(path);
+            }
+            for(int v:graph[u]) {
+                path.push_back(v);
+                dfs(v, path);
+                path.pop_back();
+            }
+        };
+
+        vector<int> path = {0};
+        dfs(0,path);
+        return ret;
+    }
+};
+
+    // def allPathsSourceTarget(self, graph: List[List[int]]) -> List[List[int]]:
+    //     self.ret = []
+    //     def dfs(u: int, t: int, path: List[int]):
+    //         if u == t:
+    //             self.ret.append(path)
+    //         for v in graph[u]:
+    //             dfs(v, t, path + [v])
+    //     dfs(0, len(graph) - 1, [0])
+    //     return self.ret
