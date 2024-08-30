30e0fb1ffa7d14e76600652407e3ab1d5f10efb0
afoteas <andreasfoteas@hotmail.com>
2024-08-31 02:39:15 +0300

    Time: 20 ms (22.6%), Space: 18.4 MB (19.87%) - LeetHub

--- 0813-all-paths-from-source-to-target/0813-all-paths-from-source-to-target.cpp
+++ 0813-all-paths-from-source-to-target/0813-all-paths-from-source-to-target.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    vector<vector<int>> allPathsSourceTarget(vector<vector<int>>& graph) {
+    vector<vector<int>> allPathsSourceTargetDfs(vector<vector<int>>& graph) {
         int t = graph.size()-1;
         vector<vector<int>> ret;
         function<void(int, vector<int>)> dfs = [&](int u, vector<int> path) { 
@@ -16,14 +16,28 @@ public:
         dfs(0,{0});
         return ret;
     }
+
+    vector<vector<int>> allPathsSourceTarget(vector<vector<int>>& graph) {
+        int size = graph.size()-1;
+        vector<vector<int>> ret;
+        deque<vector<int>> bfs(1,vector(1,0));
+        while(!bfs.empty()) {
+            vector<int> temp = bfs.front();
+            bfs.pop_front();
+            if(temp.back() == size) {
+                ret.push_back(temp);
+            }
+            for(auto& nei:graph[temp.back()]) {
+                temp.push_back(nei);
+                bfs.push_back(temp);
+                temp.pop_back();
+            }
+            
+        }
+        return ret;
+    }
 };
 
-    // def allPathsSourceTarget(self, graph: List[List[int]]) -> List[List[int]]:
-    //     self.ret = []
-    //     def dfs(u: int, t: int, path: List[int]):
-    //         if u == t:
-    //             self.ret.append(path)
-    //         for v in graph[u]:
-    //             dfs(v, t, path + [v])
-    //     dfs(0, len(graph) - 1, [0])
-    //     return self.ret
+            // for nei in graph[path[-1]]:
+            //     q.insert(0,path + [nei])
+
