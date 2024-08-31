e7ea29df25fbe76d0dd72d42a65fbb937c896438
afoteas <andreasfoteas@hotmail.com>
2024-08-31 03:59:59 +0300

    Time: 608 ms (41.99%), Space: 21.5 MB (53.3%) - LeetHub

--- 1039-find-the-town-judge/1039-find-the-town-judge.py
+++ 1039-find-the-town-judge/1039-find-the-town-judge.py
@@ -1,5 +1,5 @@
 class Solution:
-    def findJudge(self, n: int, trust: List[List[int]]) -> int:
+    def findJudgeMine(self, n: int, trust: List[List[int]]) -> int:
         if not trust:
             if n>1:
                 return -1
@@ -17,4 +17,20 @@ class Solution:
             return j
         else:
             return -1
+
+    def findJudge(self, n: int, trust: List[List[int]]) -> int:
+        # Create a list of of person and their trust value
+        numTrust = [0] * (n + 1)
+
+        # Increase trust value of trustee and decrease trust value of truster for each pair
+        for person1, person2 in trust:
+            numTrust[person1] -= 1
+            numTrust[person2] += 1
         
+        # Check if anyone achieves n-1 trust. This person is the town judge
+        for i in range(1, len(numTrust)):
+            if numTrust[i] == n - 1:
+                return i
+        
+        # If no one achieves n-1 trust, then there is no town judge
+        return -1
