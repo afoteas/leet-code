bad3486ec602a7f5f47537211e4be4c67157edde
afoteas <andreasfoteas@hotmail.com>
2024-09-01 17:24:23 +0300

    Time: 30 ms (29.65%), Space: 11.4 MB (65.7%) - LeetHub

---
+++ 0547-number-of-provinces/0547-number-of-provinces.c
@@ -0,0 +1,22 @@
+void dfs(int i, int** isConnected, int len, int* visit) {
+    for(int j=0; j<len; ++j) {
+        if(isConnected[i][j] && !visit[j]) {
+            visit[j] = 1;
+            dfs(j, isConnected, len, visit);
+        }
+    }
+}
+int findCircleNum(int** isConnected, int isConnectedSize, int* isConnectedColSize) {
+    int count = 0;
+    int* visit = (int*)calloc(isConnectedSize, sizeof(int));
+    for(int i=0; i<isConnectedSize;i++)
+    {
+        if(!visit[i])
+        {
+            count++;
+            visit[i]=1;
+            dfs(i,isConnected,isConnectedSize,visit);
+        }
+    }
+    return count;  
+}
