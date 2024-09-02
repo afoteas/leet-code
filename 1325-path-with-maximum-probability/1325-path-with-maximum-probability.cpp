fced262f408a796e86cff81696b5d5bf1c842ae0
afoteas <andreasfoteas@hotmail.com>
2024-09-02 20:34:37 +0300

    Time: 96 ms (78.96%), Space: 53.3 MB (90.92%) - LeetHub

--- 1325-path-with-maximum-probability/1325-path-with-maximum-probability.cpp
+++ 1325-path-with-maximum-probability/1325-path-with-maximum-probability.cpp
@@ -63,7 +63,7 @@ public:
         
     }
 
-    double maxProbability(int n, vector<vector<int>>& edges, vector<double>& succProb, int start_node, int end_node) {
+    double maxProbabilityBetter(int n, vector<vector<int>>& edges, vector<double>& succProb, int start_node, int end_node) {
         vector<vector<pair<double, int>>> adjList(n, vector<pair<double, int>>());
 
         for (int i = 0; i < edges.size(); i++) {
@@ -104,5 +104,32 @@ public:
             }
         }
         return 0;
+    }
+
+
+        double maxProbability(int n, vector<vector<int>>& edges, vector<double>& succProb, int start_node, int end_node) {
+        vector<double> maxProb(n, 0.0);
+        maxProb[start_node] = 1.0;
+
+        for (int i = 0; i < n - 1; ++i) {
+            bool updated = false;
+            for (int j = 0; j < edges.size(); ++j) {
+                int u = edges[j][0];
+                int v = edges[j][1];
+                double prob = succProb[j];
+
+                if (maxProb[u] * prob > maxProb[v]) {
+                    maxProb[v] = maxProb[u] * prob;
+                    updated = true;
+                }
+                if (maxProb[v] * prob > maxProb[u]) {
+                    maxProb[u] = maxProb[v] * prob;
+                    updated = true;
+                }
+            }
+            if (!updated) break;
+        }
+
+        return maxProb[end_node];
     }
 };
