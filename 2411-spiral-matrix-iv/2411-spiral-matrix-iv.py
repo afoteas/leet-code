a7ae415e247bc3a851fa1959f8d353e0c19dba3a
afoteas <andreasfoteas@hotmail.com>
2024-09-09 12:21:32 +0300

    Time: 685 ms (48.7%), Space: 56.2 MB (42.17%) - LeetHub

--- 2411-spiral-matrix-iv/2411-spiral-matrix-iv.py
+++ 2411-spiral-matrix-iv/2411-spiral-matrix-iv.py
@@ -14,28 +14,28 @@ class Solution:
                     matrix[n_min][i]=head.val
                     head = head.next
                 else:
-                    break
+                    return matrix
             n_min+=1
             for i in range(n_min, n_max+1):
                 if head:
                     matrix[i][m_max]=head.val
                     head = head.next
                 else:
-                    break
+                    return matrix
             m_max-=1
             for i in range(m_max, m_min-1, -1):
                 if head:
                     matrix[n_max][i]=head.val
                     head = head.next
                 else:
-                    break
+                    return matrix
             n_max-=1
             for i in range(n_max, n_min-1,-1):
                 if head:
                     matrix[i][m_min]=head.val
                     head = head.next
                 else:
-                    break
+                    return matrix
             m_min+=1
 
         return matrix
