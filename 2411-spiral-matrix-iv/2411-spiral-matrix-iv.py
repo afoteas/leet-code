6768abdae868f22a189c99c05b8d9a73c41bfd66
afoteas <andreasfoteas@hotmail.com>
2024-09-09 12:20:05 +0300

    Time: 659 ms (66.81%), Space: 56.3 MB (39.28%) - LeetHub

--- 2411-spiral-matrix-iv/2411-spiral-matrix-iv.py
+++ 2411-spiral-matrix-iv/2411-spiral-matrix-iv.py
@@ -6,35 +6,34 @@
 class Solution:
     def spiralMatrix(self, m: int, n: int, head: Optional[ListNode]) -> List[List[int]]:
         matrix = [[-1 for _ in range(n)] for _ in range(m)]
-        temp = head
         m_min, m_max = 0, n-1
         n_min, n_max = 0, m-1
-        while temp:
+        while head:
             for i in range(m_min, m_max+1):
-                if temp:
-                    matrix[n_min][i]=temp.val
-                    temp = temp.next
+                if head:
+                    matrix[n_min][i]=head.val
+                    head = head.next
                 else:
                     break
             n_min+=1
             for i in range(n_min, n_max+1):
-                if temp:
-                    matrix[i][m_max]=temp.val
-                    temp = temp.next
+                if head:
+                    matrix[i][m_max]=head.val
+                    head = head.next
                 else:
                     break
             m_max-=1
             for i in range(m_max, m_min-1, -1):
-                if temp:
-                    matrix[n_max][i]=temp.val
-                    temp = temp.next
+                if head:
+                    matrix[n_max][i]=head.val
+                    head = head.next
                 else:
                     break
             n_max-=1
             for i in range(n_max, n_min-1,-1):
-                if temp:
-                    matrix[i][m_min]=temp.val
-                    temp = temp.next
+                if head:
+                    matrix[i][m_min]=head.val
+                    head = head.next
                 else:
                     break
             m_min+=1
