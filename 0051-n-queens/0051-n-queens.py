15f4a031a85b0406d222754c31e6c6c37edca6dc
afoteas <andreasfoteas@hotmail.com>
2024-09-10 16:43:27 +0300

    Time: 53 ms (59.12%), Space: 17.1 MB (15.75%) - LeetHub

--- 0051-n-queens/0051-n-queens.py
+++ 0051-n-queens/0051-n-queens.py
@@ -1,5 +1,5 @@
 class Solution:
-    def solveNQueens(self, n: int) -> List[List[str]]:
+    def solveNQueensSlow(self, n: int) -> List[List[str]]:
         board = [["."]*n for _ in range(n)]
         self.ret = []
 
@@ -40,4 +40,35 @@ class Solution:
         return self.ret
 
 
+    def solveNQueens(self, n: int) -> List[List[str]]:
+        result = []
+        cols = [0] * n
+        diags_l = [0] * (2*n - 1)
+        diags_r = [0] * (2*n - 1)
+        curr_config = []
+
+        def backtrack(row:int):
+            if row == n:
+                result.append(["".join(['Q' if c == curr_config[r] else '.' for c in range(n)]) for r in range(n)])
+                return
+            
+            for i in range(n):
+                if cols[i] or diags_l[row + i] or diags_r[row + n - i - 1]:
+                    continue
+
+                cols[i] = 1
+                diags_l[row + i] = 1
+                diags_r[row + n - i - 1] = 1
+                curr_config.append(i)
+
+                backtrack(row + 1)
+
+                cols[i] = 0
+                diags_l[row + i] = 0
+                diags_r[row + n - i - 1] = 0
+                curr_config.pop()
+            
+        backtrack(0)
+
+        return result
         
