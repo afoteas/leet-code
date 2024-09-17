0e0fbcf6b656261eab98089c04798459288e8a93
afoteas <andreasfoteas@hotmail.com>
2024-09-17 21:23:01 +0300

    Time: 2323 ms (5.01%), Space: 20.1 MB (93.08%) - LeetHub

---
+++ 0015-3sum/0015-3sum.py
@@ -0,0 +1,53 @@
+class Solution:
+    def threeSumTimeout(self, nums: List[int]) -> List[List[int]]:
+        ret = set()
+        for i in range(0,len(nums)):
+            for j in range(i+1, len(nums)):
+                for k in range(j+1, len(nums)):
+                    if nums[i] + nums[j] + nums[k] == 0:
+                        ret.add(tuple(sorted((nums[i], nums[j], nums[k]))))
+        return list(ret)
+
+    def threeSumNot(self, nums: List[int]) -> List[List[int]]:
+        oneDict = defaultdict(list)
+        for i in range(len(nums)):
+            oneDict[nums[i]].append(i)
+        ret = set()
+
+        # twoDict = defaultdict(list)
+        for i in range(len(nums)):
+            for j in range(i+1, len(nums)):
+                t = -nums[i]-nums[j]
+                if t in oneDict:
+                    for k in oneDict[t]:
+                        if not k in [i,j]:
+                            ret.add(tuple(sorted((nums[i], nums[j], t))))
+        
+        return list(ret)
+
+
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+        ret = set()
+        nums.sort()
+        n=len(nums)
+        for i in range(n-1):
+            # if i > 0 and nums[i]
+            j=i+1
+            k=n-1
+            while j<k:
+                target = nums[j] + nums[k] + nums[i]
+                if target == 0:
+                    ret.add(tuple(sorted((nums[i], nums[j], nums[k]))))
+                    k-=1
+                    j+=1
+                elif target>0:
+                    k-=1
+                else:
+                    j+=1      
+        return list(ret)
+            
+
+
+
+
+        
