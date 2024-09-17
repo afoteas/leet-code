1969ded3fce47782bc6a94933d7ab9005751ba11
afoteas <andreasfoteas@hotmail.com>
2024-09-17 22:08:09 +0300

    Time: 577 ms (87.55%), Space: 20.9 MB (18.89%) - LeetHub

--- 0015-3sum/0015-3sum.py
+++ 0015-3sum/0015-3sum.py
@@ -74,13 +74,14 @@ class Solution:
 
             l, r = i + 1, len(nums) - 1
             while l < r:
-                threeSum = a + nums[l] + nums[r]
+                b, c = nums[l], nums[r]
+                threeSum = a + b + c
                 if threeSum > 0:
                     r -= 1
                 elif threeSum < 0:
                     l += 1
                 else:
-                    res.append([a, nums[l], nums[r]])
+                    res.append([a, b, c])
                     l += 1
                     r -= 1
                     while nums[l] == nums[l - 1] and l < r:
