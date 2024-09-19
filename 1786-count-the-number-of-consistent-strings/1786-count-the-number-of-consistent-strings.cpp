bc6a80e7bd68426e7fc3e7b16669e6a4f8da6bbe
afoteas <andreasfoteas@hotmail.com>
2024-09-20 01:31:57 +0300

    Time: 54 ms (24.95%), Space: 33.8 MB (85.27%) - LeetHub

---
+++ 1786-count-the-number-of-consistent-strings/1786-count-the-number-of-consistent-strings.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int countConsistentStrings(string allowed, vector<string>& words) {
+    int ret = 0;
+    for(auto& w: words) {
+        bool found = true;
+        for(char c: w){
+            if (allowed.find(c) == string::npos) {
+                found = false;
+                break;
+            }
+        }
+        if (found) ret++;
+    }
+    return ret;
+        
+    }
+};
