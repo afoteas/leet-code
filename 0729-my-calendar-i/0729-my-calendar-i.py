69f0fea4e45f5766a9c907c076458d55c922e00e
afoteas <andreasfoteas@hotmail.com>
2024-09-26 21:17:49 +0300

    Time: 238 ms (48.09%), Space: 17.5 MB (34.88%) - LeetHub

---
+++ 0729-my-calendar-i/0729-my-calendar-i.py
@@ -0,0 +1,37 @@
+import bisect
+
+class MyCalendar:
+
+    def __init__(self):
+        self.starts = []
+        self.ends = []
+        
+
+    def book(self, start: int, end: int) -> bool:
+        print(f"try: {start}, {end}")
+        if start in self.starts:
+            return False
+        
+        if not self.starts:
+            bisect.insort(self.starts, start)
+            bisect.insort(self.ends, end)
+            return True
+
+        if start >= self.ends[-1] or end <= self.starts[0]:
+            bisect.insort(self.starts, start)
+            bisect.insort(self.ends, end)
+            return True
+        pos1 = bisect.bisect_right(self.ends, start)
+        pos2 = bisect.bisect_left(self.starts, end)
+        if pos1 == pos2:
+            bisect.insort(self.starts, start)
+            bisect.insort(self.ends, end)
+            return True
+        return False
+
+        
+
+
+# Your MyCalendar object will be instantiated and called as such:
+# obj = MyCalendar()
+# param_1 = obj.book(start,end)
