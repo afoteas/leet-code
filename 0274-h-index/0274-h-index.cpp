381a9d77b3bc83ac87a5176064148d35c7364316
afoteas <andreasfoteas@hotmail.com>
2024-09-27 11:03:02 +0300

    Time: 5 ms (32.05%), Space: 11.1 MB (30.88%) - LeetHub

--- 0274-h-index/0274-h-index.cpp
+++ 0274-h-index/0274-h-index.cpp
@@ -22,7 +22,7 @@ public:
     int hIndex(vector<int>& citations) {
         sort(citations.begin(),citations.end());
         int n = citations.size();
-        for(int i =0;i<citations.size();i++){
+        for(int i =0; i<n; i++){
             if(n - i <= citations[i]){
                 return n - i;
             }
