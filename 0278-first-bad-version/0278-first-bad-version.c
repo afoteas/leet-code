baf9b2316d7ecf39f4a0eb14fb454b64e32e4d03
afoteas <andreasfoteas@hotmail.com>
2024-09-27 11:54:54 +0300

    Time: 0 ms (100%), Space: 7.9 MB (22.72%) - LeetHub

--- 0278-first-bad-version/0278-first-bad-version.c
+++ 0278-first-bad-version/0278-first-bad-version.c
@@ -3,9 +3,12 @@
 
 int firstBadVersion(int n) {
     unsigned int good = -1, bad = n, test;
+
+    printf("%u\n",good);
+    printf("%u\n",good+1);
     
     while (good+1 != bad){
-        test = (good + bad)>>1;
+        test = (good + bad)>>1u;
         if (isBadVersion(test)) bad = test;
         else good = test;
     }
