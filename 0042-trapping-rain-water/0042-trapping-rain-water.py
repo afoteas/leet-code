f08cde057eedab7ad00ffa359ebc6c87ea5b4029
afoteas <andreasfoteas@hotmail.com>
2024-10-05 16:20:24 +0300

    Time: 123 ms (43.5%), Space: 18.5 MB (49.61%) - LeetHub

---
+++ 0042-trapping-rain-water/0042-trapping-rain-water.py
@@ -0,0 +1,48 @@
+class Solution:
+    def trap(self, height: List[int]) -> int:
+        peaks = []
+        if len(height) < 3:
+            return 0
+        monotony = -1
+        previous = height[0]
+        for i,h in enumerate(height):
+            # print(f"previous={previous}, h={h}, monotony={monotony}")
+            if h <= previous and monotony >= 0:
+                peaks.append(i-1)
+            if h > previous:
+                monotony = 1
+            elif h < previous:
+                monotony = -1
+            else:
+                monotony = 0
+            previous = h
+        if height[-2] < height[-1]:
+            peaks.append(len(height)-1)
+        print(peaks)
+        tmp = [peaks[0]]
+        for i in range(1,len(peaks)-1):
+            if height[peaks[i]] <=  height[peaks[i-1]] and height[peaks[i]] <= height[peaks[i+1]]:
+                continue
+            tmp.append(peaks[i])
+        tmp.append(peaks[-1])
+        while tmp != peaks:
+            peaks = list(tmp)
+            tmp = [peaks[0]]
+            for i in range(1,len(peaks)-1):
+                if height[peaks[i]] <=  height[peaks[i-1]] and height[peaks[i]] <= height[peaks[i+1]]:
+                    continue
+                tmp.append(peaks[i])
+            tmp.append(peaks[-1])
+        print(tmp)
+        ret = 0
+        for i in range(len(peaks)-1):
+            l = peaks[i]
+            r = peaks[i+1]
+            h = min(height[l], height[r])
+            for j in range(l,r+1):
+                w = h - height[j]
+                if w > 0:
+                    ret+=w
+        return ret
+
+        
