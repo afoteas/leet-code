27f27542bc9fd0dbf5a4e6f2f2dfadfabc1973c7
afoteas <andreasfoteas@hotmail.com>
2024-10-05 13:20:06 +0300

    Time: 9 ms (5.81%), Space: 14.5 MB (35.38%) - LeetHub

--- 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.cpp
+++ 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.cpp
@@ -11,7 +11,7 @@
  */
 class Solution {
 public:
-    vector<int> rightSideView(TreeNode* root) {
+    vector<int> rightSideViewSlow(TreeNode* root) {
         vector<int> ret;
         if (root == NULL) {
             return ret;
@@ -22,30 +22,35 @@ public:
             if (node->left) addLevels(node->left,level+1);
             if (node->right) addLevels(node->right,level+1);
         };
-        addLevels(root,0);
+        addLevels(root, 0);
         for(auto &level: levels) {
             ret.push_back(level.second.back());
         }
         return ret;
     }
+
+        vector<int> rightSideView(TreeNode* root) {
+        vector<int> view;
+        if (!root) return view;
+
+        queue<TreeNode*> q;
+        q.push(root);
+
+        while (!q.empty()) {
+            int countAtLevel = q.size();
+
+            for (int i = 0; i < countAtLevel; i++) {
+                TreeNode* node = q.front();
+                q.pop();
+
+                // If we are at the right-most node in the level, add it to view
+                if (i == countAtLevel - 1) view.push_back(node->val);
+
+                if (node->left) q.push(node->left);
+                if (node->right) q.push(node->right);
+            }
+        }
+
+        return view;
+    }
 };
-
-
-// def rightSideView(self, root: Optional[TreeNode]) -> List[int]:
-//     if not root:
-//         return []
-//     ret = []
-//     levels = {}
-//     def addLevels(node:Optional[TreeNode],level:int):
-//         if level in levels:
-//             levels[level].append(node.val)
-//         else:
-//             levels[level] = [node.val]
-//         if node.left:
-//             addLevels(node.left,level+1)
-//         if node.right:
-//             addLevels(node.right,level+1)
-//     addLevels(root,0)
-//     for values in levels.values():
-//         ret.append(values[-1])
-//     return ret
