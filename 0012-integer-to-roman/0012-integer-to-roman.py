985a5daab9228717b2230d0bf0d63c4c31f4f4c8
afoteas <andreasfoteas@hotmail.com>
2024-10-07 00:17:22 +0300

    Time: 64 ms (5.01%), Space: 16.6 MB (22.64%) - LeetHub

--- 0012-integer-to-roman/0012-integer-to-roman.py
+++ 0012-integer-to-roman/0012-integer-to-roman.py
@@ -5,6 +5,6 @@ class Solution:
          "L":50, "XL":40, "X":10, "IX":9, "V":5, "IV":4, "I":1}
         for number in values:
             count = num // values[number]
-            num = num - values[number]*count
+            num -= values[number]*count
             returnStr += number*count
         return returnStr
