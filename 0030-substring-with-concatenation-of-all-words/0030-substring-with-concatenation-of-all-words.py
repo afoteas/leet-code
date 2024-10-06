6dd25694226f72c241766cd833488ff000730b87
afoteas <andreasfoteas@hotmail.com>
2024-10-06 23:55:21 +0300

    Time: 2119 ms (51.73%), Space: 17.5 MB (15.9%) - LeetHub

--- 0030-substring-with-concatenation-of-all-words/0030-substring-with-concatenation-of-all-words.py
+++ 0030-substring-with-concatenation-of-all-words/0030-substring-with-concatenation-of-all-words.py
@@ -1,5 +1,6 @@
+from collections import Counter
 class Solution:
-    def findSubstring(self, s: str, words: List[str]) -> List[int]:
+    def findSubstringMine(self, s: str, words: List[str]) -> List[int]:
         l = 0
         r = 0
         ret = []
@@ -25,5 +26,33 @@ class Solution:
             l+=1
         return ret
                 
-
+    def findSubstring(self, s: str, words: List[str]) -> List[int]:
+        l = 0
+        r = 0
+        ret = []
+        if len(set(words)) == 1:
+            words = [''.join(words)]
+        m = len(s)
+        n = len(words[0])
+        t = n*len(words)
+        if t>m:
+            return ret
+        word_count = Counter(words) 
+        while l + n <= m:
+            if s[l:l+n] in words:
+                seen_words = Counter()
+                seen_words[s[l:l+n]] += 1
+                r=n
+                while seen_words != word_count:
+                    if s[l+r:l+r+n] in word_count:
+                        seen_words[s[l+r:l+r+n]] += 1
+                        if seen_words[s[l+r:l+r+n]] > word_count[s[l+r:l+r+n]]:
+                            break
+                        r+=n
+                    else:
+                        break
+                if seen_words == word_count:
+                    ret.append(l)
+            l+=1
+        return ret
 
