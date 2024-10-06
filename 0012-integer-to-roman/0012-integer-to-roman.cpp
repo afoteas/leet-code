bb7dc422f4dffbb598bdb82a8529469025404163
afoteas <andreasfoteas@hotmail.com>
2024-10-07 00:27:03 +0300

    Time: 11 ms (32.12%), Space: 13.7 MB (30.79%) - LeetHub

--- 0012-integer-to-roman/0012-integer-to-roman.cpp
+++ 0012-integer-to-roman/0012-integer-to-roman.cpp
@@ -14,14 +14,3 @@ public:
          return returnStr;
     }
 };
-
-
-    // def intToRoman(self, num: int) -> str:
-    //     returnStr = ""
-    //     values = {"M":1000, "CM":900, "D":500, "CD":400, "C":100, "XC":90,
-    //      "L":50, "XL":40, "X":10, "IX":9, "V":5, "IV":4, "I":1}
-    //     for number in values:
-    //         count = num // values[number]
-    //         num = num - values[number]*count
-    //         returnStr += number*count
-    //     return returnStr
