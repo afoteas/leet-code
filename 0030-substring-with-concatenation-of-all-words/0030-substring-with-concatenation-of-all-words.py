cdb60c7d57f2f350b627d54d9a113bc87c89a55e
afoteas <andreasfoteas@hotmail.com>
2024-10-06 23:12:20 +0300

    Time: 1382 ms (52.51%), Space: 17.3 MB (72.8%) - LeetHub

--- 0030-substring-with-concatenation-of-all-words/0030-substring-with-concatenation-of-all-words.py
+++ 0030-substring-with-concatenation-of-all-words/0030-substring-with-concatenation-of-all-words.py
@@ -2,15 +2,14 @@ class Solution:
     def findSubstring(self, s: str, words: List[str]) -> List[int]:
         m = len(s)
         n = len(words[0])
-        print(m)
-        print(len(words))
         l = 0
         r = 0
         ret = []
-        if m == len(words):
-            return [0]
-        if len(words) == 5000:
-            return [i for i in range(5001)]
+        # if m == len(words):
+        #     return [0]
+        if len(set(words)) == 1:
+            words = [''.join(words)]
+        n = len(words[0])
         while l + n <= m:
             if s[l:l+n] in words:
                 tmp = list(words)
