821374e1b30ad057a7e179663061d94e59ef2007
afoteas <andreasfoteas@hotmail.com>
2024-10-07 00:44:01 +0300

    Time: 11 ms (44.79%), Space: 25.8 MB (12.66%) - LeetHub

--- 0108-convert-sorted-array-to-binary-search-tree/0108-convert-sorted-array-to-binary-search-tree.cpp
+++ 0108-convert-sorted-array-to-binary-search-tree/0108-convert-sorted-array-to-binary-search-tree.cpp
@@ -12,21 +12,13 @@
 class Solution {
 public:
     TreeNode* sortedArrayToBST(vector<int>& nums) {
-        return buildBST(nums, 0, nums.size() - 1);  // Pass the full array with indices
-    }
-
-private:
-    TreeNode* buildBST(vector<int>& nums, int left, int right) {
-        if (left > right) {
-            return nullptr;  // Base case: no elements in this range
+        int len = nums.size();
+        if (len == 0) {
+            return nullptr;
         }
-
-        int median = left + (right - left) / 2;  // Avoid overflow for large numbers
-        TreeNode* node = new TreeNode(nums[median]);  // Create the root node with median value
-
-        node->left = buildBST(nums, left, median - 1);  // Recursively build left subtree
-        node->right = buildBST(nums, median + 1, right);  // Recursively build right subtree
-
-        return node;
+        int median = len/2;
+        vector<int> first_half(nums.begin(), nums.begin() + median);
+        vector<int> second_half(nums.begin() + median + 1, nums.end());
+        return new TreeNode(nums[median], sortedArrayToBST(first_half), sortedArrayToBST(second_half));
     }
 };
