b01e5d1078db7af5b2ad9a3b80bf65b1251f3cec
afoteas <andreasfoteas@hotmail.com>
2024-10-07 00:46:16 +0300

    Time: 14 ms (20.39%), Space: 22.1 MB (86.51%) - LeetHub

--- 0108-convert-sorted-array-to-binary-search-tree/0108-convert-sorted-array-to-binary-search-tree.cpp
+++ 0108-convert-sorted-array-to-binary-search-tree/0108-convert-sorted-array-to-binary-search-tree.cpp
@@ -11,7 +11,7 @@
  */
 class Solution {
 public:
-    TreeNode* sortedArrayToBST(vector<int>& nums) {
+    TreeNode* sortedArrayToBSTmine(vector<int>& nums) {
         int len = nums.size();
         if (len == 0) {
             return nullptr;
@@ -20,5 +20,23 @@ public:
         vector<int> first_half(nums.begin(), nums.begin() + median);
         vector<int> second_half(nums.begin() + median + 1, nums.end());
         return new TreeNode(nums[median], sortedArrayToBST(first_half), sortedArrayToBST(second_half));
+    }
+    TreeNode* sortedArrayToBST(vector<int>& nums) {
+        return buildBST(nums, 0, nums.size() - 1);  // Pass the full array with indices
+    }
+
+private:
+    TreeNode* buildBST(vector<int>& nums, int left, int right) {
+        if (left > right) {
+            return nullptr;  // Base case: no elements in this range
+        }
+
+        int median = left + (right - left) / 2;  // Avoid overflow for large numbers
+        TreeNode* node = new TreeNode(nums[median]);  // Create the root node with median value
+
+        node->left = buildBST(nums, left, median - 1);  // Recursively build left subtree
+        node->right = buildBST(nums, median + 1, right);  // Recursively build right subtree
+
+        return node;
     }
 };
