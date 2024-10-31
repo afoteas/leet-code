d2dba886e39057cc649b0cbe0b49879f319de674
afoteas <andreasfoteas@hotmail.com>
2024-11-01 00:09:51 +0200

    Time: 167 ms (16.93%), Space: 183.6 MB (5.03%) - LeetHub

--- 2646-kth-largest-sum-in-a-binary-tree/2646-kth-largest-sum-in-a-binary-tree.cpp
+++ 2646-kth-largest-sum-in-a-binary-tree/2646-kth-largest-sum-in-a-binary-tree.cpp
@@ -38,26 +38,3 @@ public:
         
     }
 };
-
-/*
-class Solution:
-    def kthLargestLevelSum(self, root: Optional[TreeNode], k: int) -> int:
-        levels = []
-        def kfun(node: Optional[TreeNode], level:int):
-            if len(levels) == level:
-                levels.append(node.val)
-            else:
-                levels[level] += node.val
-
-            if node.left:
-                kfun(node.left, level+1)
-            if node.right:
-                kfun(node.right, level+1)
-            
-        kfun(root, 0)
-        if len(levels) < k:
-            return -1
-        levels.sort(reverse = True)
-        return levels[k-1]
-
-*/
