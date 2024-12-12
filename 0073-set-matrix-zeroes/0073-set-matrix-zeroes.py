738de7ef90b31d42d0a3b6fb2e806b3361e8a4e1
afoteas <andreasfoteas@hotmail.com>
2024-12-12 17:09:36 +0200

    Time: 4 ms (41.95%), Space: 18 MB (11.1%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.py
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.py
@@ -1,5 +1,5 @@
 class Solution:
-    def setZeroes(self, matrix: List[List[int]]) -> None:
+    def setZeroesOld(self, matrix: List[List[int]]) -> None:
         """
         Do not return anything, modify matrix in-place instead.
         """
@@ -17,5 +17,30 @@ class Solution:
             else:
                 for jj in verticals:
                     matrix[i][jj] = 0
+
+    def setZeroes(self, matrix: List[List[int]]) -> None:
+        """
+        Do not return anything, modify matrix in-place instead.
+        """
+        m = len(matrix)
+        n = len(matrix[0])
+        verticals = set()
+        for i in range(m):
+            new = []
+            hasZero = False
+            for index, value in enumerate(matrix[i]):
+                if index in verticals:
+                    matrix[i][index] = 0
+                else:
+                    if value == 0:
+                        new.append(index)
+                if value == 0:
+                    hasZero = True
+            verticals.update(new)
+            for ii in range(0,i):
+                for jj in new:
+                    matrix[ii][jj] = 0
+            if hasZero:
+                matrix[i] = [0]*n
 
         
