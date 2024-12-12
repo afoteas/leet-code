878cfcd706d9ec26684b2191dc7b1e3497ea51e7
afoteas <andreasfoteas@hotmail.com>
2024-12-12 16:54:39 +0200

    Time: 6 ms (26.16%), Space: 17.8 MB (20.89%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.py
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.py
@@ -5,10 +5,10 @@ class Solution:
         """
         m = len(matrix)
         n = len(matrix[0])
-        verticals = set()
+        verticals = []
         for i in range(m):
             newVerticals = [index for index, value in enumerate(matrix[i]) if value == 0 and index not in verticals]
-            verticals.update(newVerticals)
+            verticals.extend(newVerticals)
             for ii in range(0,i):
                 for jj in newVerticals:
                     matrix[ii][jj] = 0
