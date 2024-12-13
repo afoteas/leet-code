0aa90682fce34345fe7665f6fce66ad864e8912b
afoteas <andreasfoteas@hotmail.com>
2024-12-13 18:50:48 +0200

    Time: 0 ms (100%), Space: 9.8 MB (26.65%) - LeetHub

--- 1146-greatest-common-divisor-of-strings/1146-greatest-common-divisor-of-strings.cpp
+++ 1146-greatest-common-divisor-of-strings/1146-greatest-common-divisor-of-strings.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    string gcdOfStrings(string str1, string str2) {
+    string gcdOfStringsMem(string str1, string str2) {
         if (str1 + str2 != str2 + str1) {
             return "";
         }
@@ -14,5 +14,9 @@ public:
         };
         int gcdLength = gcd(str1.size(), str2.size());
         return str1.substr(0, gcdLength);        
+    }
+
+    string gcdOfStrings(string str1, string str2) {
+        return str1+str2==str2+str1?str2.substr(0,gcd(str1.length(),str2.length())):"";
     }
 };
