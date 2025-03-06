98b642d3ca23140e395b4c4a625c983357976815
afoteas <andreasfoteas@hotmail.com>
2025-03-06 20:27:04 +0200

    Time: 15 ms (31.25%), Space: 18.2 MB (45%) - LeetHub

--- 3227-find-missing-and-repeated-values/3227-find-missing-and-repeated-values.py
+++ 3227-find-missing-and-repeated-values/3227-find-missing-and-repeated-values.py
@@ -1,5 +1,5 @@
 class Solution:
-    def findMissingAndRepeatedValues(self, grid: List[List[int]]) -> List[int]:
+    def findMissingAndRepeatedValuesSlow(self, grid: List[List[int]]) -> List[int]:
         n = len(grid)
         ret = [i for i in range(1, n*n + 1)]
         for i in range(n):
@@ -7,7 +7,21 @@ class Solution:
                 if grid[i][j] in ret:
                     ret.remove(grid[i][j])
                 else:
-                    ret.insert(0,grid[i][j])
+                    ret.insert(0, grid[i][j])
         return ret
 
+    def findMissingAndRepeatedValues(self, grid: List[List[int]]) -> List[int]:
+        n = len(grid)
+        nums = [1 for i in range(1, n*n + 1)]
+        a = None
+        b = None
+        for i in range(n):
+            for j in range(n):
+                if nums[grid[i][j]-1]:
+                    nums[grid[i][j]-1] -= 1
+                else:
+                    a = grid[i][j]
+        print(nums)
+        b = nums.index(1) + 1
+        return [a, b]
         
