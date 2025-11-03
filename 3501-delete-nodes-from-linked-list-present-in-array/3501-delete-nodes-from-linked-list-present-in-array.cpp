ec860dab7a49acbce6adcd927c23cf0782c9e757
afoteas <andreasfoteas@hotmail.com>
2025-11-03 14:28:13 +0200

    Time: 170 ms (13.38%), Space: 266.2 MB (17.55%) - LeetHub

---
+++ 3501-delete-nodes-from-linked-list-present-in-array/3501-delete-nodes-from-linked-list-present-in-array.cpp
@@ -0,0 +1,41 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+ #include <set>
+ using namespace std;
+
+    //  def modifiedList(self, nums: List[int], head: Optional[ListNode]) -> Optional[ListNode]:
+    //     n = set(nums)
+    //     dummy = ListNode(0)
+    //     dummy.next = head
+    //     curr = dummy
+    //     while curr.next:
+    //         if (curr.next.val in n):
+    //             curr.next = curr.next.next
+    //         else:
+    //             curr = curr.next
+    //     return dummy.next
+class Solution {
+public:
+    ListNode* modifiedList(vector<int>& nums, ListNode* head) {
+        set<int> const  n(begin(nums), end(nums));
+        ListNode dummy(0, head);
+        ListNode* curr = &dummy;
+
+        while (curr->next) {
+            if (n.count(curr->next->val)) {
+                curr->next = curr->next->next;
+            } else {
+                curr = curr->next;
+            }
+        }
+        return dummy.next; 
+    }
+};
