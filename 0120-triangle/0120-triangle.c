26eacb0556e46a883a2495f89e3f24e3fb518824
afoteas <andreasfoteas@hotmail.com>
2025-11-04 14:40:10 +0200

    Time: 0 ms (100%), Space: 9.2 MB (77.53%) - LeetHub

---
+++ 0120-triangle/0120-triangle.c
@@ -0,0 +1,9 @@
+int minimumTotal(int** triangle, int triangleSize, int* triangleColSize) {
+    for(int i = triangleSize-2; i > -1 ; i--) {
+        for(int j = 0; j < triangleColSize[i]; j++) {
+            triangle[i][j] += triangle[i + 1][j] > triangle[i + 1][j + 1] ? triangle[i + 1][j + 1] : triangle[i + 1][j];
+        }
+    }
+    return triangle[0][0];
+    
+}
