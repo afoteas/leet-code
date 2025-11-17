ffef7e6d1440edb06194ef9108edead555603a07
afoteas <andreasfoteas@hotmail.com>
2025-11-17 15:50:42 +0200

    Time: 0 ms (100%), Space: 7.7 MB (90.39%) - LeetHub

--- 1817-calculate-money-in-leetcode-bank/1817-calculate-money-in-leetcode-bank.cpp
+++ 1817-calculate-money-in-leetcode-bank/1817-calculate-money-in-leetcode-bank.cpp
@@ -7,8 +7,7 @@ public:
         int step2 = 0;
         for (int i = 0; i<n; i++) {
             if(i%7 == 0) {
-                step1++;
-                step2 = step1;
+                step2 = ++step1;
             }
             ret += step2++;
         }
