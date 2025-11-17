bf5265d6ca184b8e121a7f412b83faee1f5efd3a
afoteas <andreasfoteas@hotmail.com>
2025-11-17 14:08:42 +0200

    Time: 0 ms (100%), Space: 7.8 MB (72.32%) - LeetHub

---
+++ 2288-count-operations-to-obtain-zero/2288-count-operations-to-obtain-zero.cpp
@@ -0,0 +1,21 @@
+class Solution {
+    int operations = 0;
+public:
+    int countOperations(int num1, int num2) {
+        // cout << "num1: " << num1 << ", num2: " << num2 << "\n";
+        if (num1 == 0 || num2 == 0) {
+            return operations;
+        }
+        if (num1 == num2) {
+            return operations +1;
+        }
+        if (num1 > num2) {
+            num1 -= num2;
+        }
+        else {
+            num2 -= num1;
+        }
+        operations++;
+        return countOperations(num1, num2);
+    }
+};
