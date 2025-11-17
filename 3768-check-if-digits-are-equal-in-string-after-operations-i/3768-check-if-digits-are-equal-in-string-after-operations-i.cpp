1818fea322573a461965297d828008d655d02a38
afoteas <andreasfoteas@hotmail.com>
2025-11-17 15:04:44 +0200

    Time: 5 ms (48.72%), Space: 10.1 MB (25.52%) - LeetHub

--- 3768-check-if-digits-are-equal-in-string-after-operations-i/3768-check-if-digits-are-equal-in-string-after-operations-i.cpp
+++ 3768-check-if-digits-are-equal-in-string-after-operations-i/3768-check-if-digits-are-equal-in-string-after-operations-i.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    bool hasSameDigits(string s) {
+    bool hasSameDigitsFirst(string s) {
         const int size = s.size();
         if(size == 2) {
             if(s[0] == s[1]) {
@@ -15,6 +15,24 @@ public:
             ss[i] = (int(s[i]) + int(s[i+1]))%10;
         }
         return hasSameDigits(ss);
+
+    }
+
+    bool hasSameDigits(string s) {
+        const int size = s.size();
+        if(size == 2) {
+            if(s[0] == s[1]) {
+                return true;
+            }
+            else {
+                return false;
+            }
+        }
+        for(int i = 0; i < size-1; i++) {
+            s[i] = (int(s[i]) + int(s[i+1]))%10;
+        }
+        s.resize(size-1);
+        return hasSameDigits(s);
 
     }
 };
