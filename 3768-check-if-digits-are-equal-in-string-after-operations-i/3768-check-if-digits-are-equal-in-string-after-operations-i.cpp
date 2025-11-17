a77f3ab80d7594a0ff9e2e9c1655aa137450b218
afoteas <andreasfoteas@hotmail.com>
2025-11-17 15:35:54 +0200

    Time: 0 ms (100%), Space: 8.2 MB (99.32%) - LeetHub

--- 3768-check-if-digits-are-equal-in-string-after-operations-i/3768-check-if-digits-are-equal-in-string-after-operations-i.cpp
+++ 3768-check-if-digits-are-equal-in-string-after-operations-i/3768-check-if-digits-are-equal-in-string-after-operations-i.cpp
@@ -18,7 +18,7 @@ public:
 
     }
 
-    bool hasSameDigits(string s) {
+    bool hasSameDigitsBetter(string s) {
         const int size = s.size();
         if(size == 2) {
             if(s[0] == s[1]) {
@@ -35,5 +35,15 @@ public:
         s.pop_back();
         return hasSameDigits(s);
 
+    }
+
+    bool hasSameDigits(string& s) {
+        const int n=s.size();
+        for(int i=n-1; i>1; i--){
+            for(int j=0; j<i; j++){
+                s[j]=(s[j]-'0'+s[j+1]-'0')%10+'0';
+            }
+        }
+        return s[0]==s[1];
     }
 };
