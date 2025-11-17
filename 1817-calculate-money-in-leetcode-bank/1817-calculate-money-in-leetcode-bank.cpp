e84ebea97a6a6665efe960d3632ffbee55cd8f96
afoteas <andreasfoteas@hotmail.com>
2025-11-17 15:52:37 +0200

    Time: 0 ms (100%), Space: 7.9 MB (56.46%) - LeetHub

--- 1817-calculate-money-in-leetcode-bank/1817-calculate-money-in-leetcode-bank.cpp
+++ 1817-calculate-money-in-leetcode-bank/1817-calculate-money-in-leetcode-bank.cpp
@@ -2,9 +2,7 @@ class Solution {
 public:
 
     int totalMoney(int n) {
-        int ret = 0;
-        int step1 = 0;
-        int step2 = 0;
+        int ret = 0, step1 = 0, step2 = 0;
         for (int i = 0; i<n; i++) {
             if(i%7 == 0) {
                 step2 = ++step1;
