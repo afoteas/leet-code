bdb6b12959b46618b950e6c03924523cee763ee8
afoteas <andreasfoteas@hotmail.com>
2025-11-20 18:16:17 +0200

    Time: 0 ms (100%), Space: 8.3 MB (26.11%) - LeetHub

---
+++ 0717-1-bit-and-2-bit-characters/0717-1-bit-and-2-bit-characters.c
@@ -0,0 +1,20 @@
+bool isOneBitCharacter(int* bits, int bitsSize) {
+
+    bool second = false;
+    bool ret = true;
+    for(int i; i < bitsSize; i++) {
+        if (second) {
+            second = false;
+            ret = false;
+        }
+        else {
+            if (bits[i]) {
+                second = true;
+            } else {
+                second = false;
+                ret = true;
+            }
+        }
+    }
+    return ret;
+}
