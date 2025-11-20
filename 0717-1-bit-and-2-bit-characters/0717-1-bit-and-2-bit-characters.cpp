4f48cae618a0b4dc8be3d26854aacfc363932d7e
afoteas <andreasfoteas@hotmail.com>
2025-11-20 18:02:10 +0200

    Time: 0 ms (100%), Space: 13 MB (76.87%) - LeetHub

--- 0717-1-bit-and-2-bit-characters/0717-1-bit-and-2-bit-characters.cpp
+++ 0717-1-bit-and-2-bit-characters/0717-1-bit-and-2-bit-characters.cpp
@@ -4,13 +4,9 @@ public:
         bool second = false;
         bool ret = false;
         for(auto& bit: bits) {
-            // cout << "bit:" << bit << ", ";
-            // cout << "second:" << second << ", ";
-            // cout << "ret:" << ret << "\n";
             if (second) {
                 second = false;
                 ret = false;
-                // cout << "\nentered!\n";
             }
             else {
                 if (bit) {
