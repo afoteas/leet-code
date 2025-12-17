0610261b581043d933501eb1648e332ab73b9bad
afoteas <andreasfoteas@hotmail.com>
2025-12-17 18:33:40 +0200

    Time: 0 ms (100%), Space: 17.8 MB (41.73%) - LeetHub

--- 3432-count-partitions-with-even-sum-difference/3432-count-partitions-with-even-sum-difference.py
+++ 3432-count-partitions-with-even-sum-difference/3432-count-partitions-with-even-sum-difference.py
@@ -1,10 +1,14 @@
 class Solution:
+    # def countPartitionsSlow(self, nums: List[int]) -> int:
+    #     ret = 0
+    #     for i in range(1,len(nums)):
+    #         # print(f"left: {sum(nums[:i])}")
+    #         # print(f"right {sum(nums[i:len(nums)])}")
+    #         if (sum(nums[:i]) - sum(nums[i:len(nums)]))%2 == 0 :
+    #             ret+=1
+    #     return ret
+
+
     def countPartitions(self, nums: List[int]) -> int:
-        ret = 0
-        for i in range(1,len(nums)):
-            # print(f"left: {sum(nums[:i])}")
-            # print(f"right {sum(nums[i:len(nums)])}")
-            if (sum(nums[:i]) - sum(nums[i:len(nums)]))%2 == 0 :
-                ret+=1
-        return ret
-        
+        totalSum = sum(nums)
+        return len(nums) - 1 if totalSum % 2 == 0 else 0
