2e94b4ca64cd663e353dd775a49f38d86f8045fb
afoteas <andreasfoteas@hotmail.com>
2025-12-22 15:09:03 +0200

    Time: 0 ms (100%), Space: 17.3 MB (99.86%) - LeetHub

---
+++ 3370-smallest-number-with-all-set-bits/3370-smallest-number-with-all-set-bits.py
@@ -0,0 +1,10 @@
+class Solution:
+    def smallestNumber(self, n: int) -> int:
+        ret = 1
+        if n == 1:
+            return 1
+        while ret <= n :
+            ret = ret<<1
+        return ret - 1
+
+        
