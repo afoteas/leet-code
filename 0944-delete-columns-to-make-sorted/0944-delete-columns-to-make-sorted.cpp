70c6ccfaa01fbf2a89af7a63ec072706099e71d2
afoteas <andreasfoteas@hotmail.com>
2025-12-22 15:45:57 +0200

    Time: 0 ms (100%), Space: 15.6 MB (49.46%) - LeetHub

--- 0944-delete-columns-to-make-sorted/0944-delete-columns-to-make-sorted.cpp
+++ 0944-delete-columns-to-make-sorted/0944-delete-columns-to-make-sorted.cpp
@@ -3,19 +3,15 @@ public:
     int minDeletionSize(vector<string>& strs) {
         int strlen = strs[0].size();
         int veclen = strs.size();
-        cout << strlen << endl;
         int ret = 0;
 
         for(int i=0; i < strlen; i++) {
-            for (int j = 0; j < veclen; j++) {
-                // cout << strs[i][j] << " ";
-                if (j > 0 && strs[j-1][i] > strs[j][i]) {
+            for (int j = 1; j < veclen; j++) {
+                if (strs[j-1][i] > strs[j][i]) {
                     ret++;
                     break;
                 }
             }
-            // cout << endl;
-
         }
 
         return ret;
