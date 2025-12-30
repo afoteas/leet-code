5a169f95a886cc59d6fba55333680b5539e3f7d1
afoteas <andreasfoteas@hotmail.com>
2025-12-30 18:13:40 +0200

    Time: 34 ms (76.51%), Space: 17.2 MB (99.37%) - LeetHub

--- 1523-count-odd-numbers-in-an-interval-range/1523-count-odd-numbers-in-an-interval-range.py
+++ 1523-count-odd-numbers-in-an-interval-range/1523-count-odd-numbers-in-an-interval-range.py
@@ -4,12 +4,9 @@ class Solution:
             print("here")
             return int((high-low+1)/2)
         else:
-            # return int((high-low+1)/2) if low %2 == 1 else int((high-low)/2)
             if low %2 == 1:
-                print("here1")
                 return int((high-low)/2)+1
             else:
-                print("here2")
                 return int((high-low)/2)
 
         
