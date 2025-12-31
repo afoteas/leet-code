29266d8cbeed1470afd646a77cd1efb4a44851de
afoteas <andreasfoteas@hotmail.com>
2025-12-31 02:40:41 +0200

    Time: 0 ms (100%), Space: 7.9 MB (36.05%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.cpp
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -1,10 +1,25 @@
 class Solution {
 public:
-    int fib(int n) {
+    int fibr(int n) {
         if (n < 2) {
             return n;
         } else {
-            return fib(n-2) + fib(n-1);
+            return fibr(n-2) + fibr(n-1);
         }
+    }
+
+    int fib(int n) {
+        int pre=0;
+        int ret=1;
+        if (n < 2) {
+            return n;
+        }
+        for(int i=2 ; i <=n ; i++){
+            int temp = ret;
+            ret = ret + pre;
+            pre = temp;
+        }
+        return ret;
+
     }
 };
