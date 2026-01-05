b94445090f4f4d34c7ac72eeefdec9890301a97e
afoteas <andreasfoteas@hotmail.com>
2026-01-05 02:11:24 +0200

    Time: 0 ms (100%), Space: 13.9 MB (91.85%) - LeetHub

--- 2154-keep-multiplying-found-values-by-two/2154-keep-multiplying-found-values-by-two.cpp
+++ 2154-keep-multiplying-found-values-by-two/2154-keep-multiplying-found-values-by-two.cpp
@@ -1,8 +1,15 @@
 class Solution {
 public:
-    int findFinalValue(vector<int>& nums, int original) {
+    int findFinalValueSet(vector<int>& nums, int original) {
         set<int> nn(nums.begin(), nums.end());
         while (nn.contains(original)) {
+            original <<=1;
+        }
+        return original;
+    }
+
+    int findFinalValue(vector<int>& nums, int original) {
+        while (find(nums.begin(),nums.end(),original) != nums.end()) {
             original <<=1;
         }
         return original;
