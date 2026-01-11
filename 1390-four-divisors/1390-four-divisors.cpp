d5dd0bd59ce45ee56186bdb0ee3b37d9558fe7d5
afoteas <andreasfoteas@hotmail.com>
2026-01-11 22:23:44 +0200

    Time: 4 ms (86.11%), Space: 16.3 MB (33.2%) - LeetHub

--- 1390-four-divisors/1390-four-divisors.cpp
+++ 1390-four-divisors/1390-four-divisors.cpp
@@ -1,13 +1,13 @@
 class Solution {
 public:
-    int sumFourDivisors(vector<int>& nums) {
+    int sumFourDivisorsMine(vector<int>& nums) {
         int ret = 0;
         // map<int, vector<int>> memo;
         for (auto& num: nums) {
             int max = sqrt(num);
             // cout << "max =" << max << endl;
             set<int> divisors {1, num};
-            for(int i = 2; i <= max; i++) {
+            for(int i = 2; i <= max; ++i) {
                 int rest = num%i;
                 int divisor = num/i;
                 if (rest == 0) {
@@ -20,5 +20,34 @@ public:
             }
         }
         return ret;
+    }
+
+
+    int sumFourDivisors(vector<int>& nums) {
+        int ans = 0;
+
+        for (int x : nums) {
+            int cnt = 0;
+            int sum = 0;
+
+            for (int d = 1; d * d <= x; d++) {
+                if (x % d == 0) {
+                    int e = x / d;
+                    cnt++;
+                    sum += d;
+
+                    if (e != d) {
+                        cnt++;
+                        sum += e;
+                    }
+                }
+                if (cnt > 4) break;
+            }
+
+            if (cnt == 4)
+                ans += sum;
+        }
+
+        return ans;
     }
 };
