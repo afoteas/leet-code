30d7e80b26cd8eec50f5059f5af2d4e03ba763b4
Andreas Foteas <andreasfoteas@hotmail.com>
2026-01-22 20:42:45 +0200

    Time: 7 ms (8.7%), Space: 25.1 MB (81.44%) - LeetHub

---
+++ 3314-construct-the-minimum-bitwise-array-i/3314-construct-the-minimum-bitwise-array-i.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    vector<int> minBitwiseArray(vector<int>& nums) {
+        vector<int> ans;
+        for(const auto& num: nums) {
+            bool found = false;
+            for (int i=1; i < num; ++i ) {
+                if((i|(i+1)) == num) {
+                    ans.push_back(i);
+                    found = true;
+                    break;
+                } 
+            }
+            if (!found) {
+                ans.push_back(-1);
+            }
+        }
+        
+        return ans;
+    }
+};
