53675ed6c433c8a9ed2c05f255a9c07e4743eaca
Andreas Foteas <andreasfoteas@hotmail.com>
2026-01-22 21:18:37 +0200

    Time: 10 ms (8.7%), Space: 25.2 MB (55.9%) - LeetHub

--- 3314-construct-the-minimum-bitwise-array-i/3314-construct-the-minimum-bitwise-array-i.cpp
+++ 3314-construct-the-minimum-bitwise-array-i/3314-construct-the-minimum-bitwise-array-i.cpp
@@ -4,6 +4,10 @@ public:
         vector<int> ans;
         for(const auto& num: nums) {
             bool found = false;
+            if (!num&1) {
+                ans.push_back(-1);
+                continue;
+            }
             for (int i=1; i < num; ++i ) {
                 if((i|(i+1)) == num) {
                     ans.push_back(i);
