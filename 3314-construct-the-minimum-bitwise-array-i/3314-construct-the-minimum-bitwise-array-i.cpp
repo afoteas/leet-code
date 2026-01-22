cc390217b310563d822d83044c6c6ba3c8cabdc1
Andreas Foteas <andreasfoteas@hotmail.com>
2026-01-22 21:10:50 +0200

    Time: 2 ms (39.62%), Space: 25.4 MB (9.47%) - LeetHub

--- 3314-construct-the-minimum-bitwise-array-i/3314-construct-the-minimum-bitwise-array-i.cpp
+++ 3314-construct-the-minimum-bitwise-array-i/3314-construct-the-minimum-bitwise-array-i.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    vector<int> minBitwiseArraySlow(vector<int>& nums) {
+    vector<int> minBitwiseArray(vector<int>& nums) {
         vector<int> ans;
         for(const auto& num: nums) {
             bool found = false;
@@ -14,33 +14,6 @@ public:
             if (!found) {
                 ans.push_back(-1);
             }
-        }
-        
-        return ans;
-    }
-
-    vector<int> minBitwiseArray(vector<int>& nums) {
-        vector<int> ans;
-        unordered_map<int,int> mapa;
-        int i = 1;
-        while (i < 1001) {
-            mapa.insert({(i|(i+1)),i});
-            ++i;
-        }
-        for(const auto& num: nums) {
-            auto it = mapa.find(num);
-            if (it != mapa.end()) {
-                ans.push_back(it->second);
-            }
-            else {
-                ans.push_back(-1);
-            }
-
-            // try {
-            //     ans.push_back(mapa.at(num));
-            // } catch (const std::out_of_range& e) {
-            //     ans.push_back(-1);
-            // }
         }
         
         return ans;
