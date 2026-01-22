786079ebefda861446a3b4c480dd62ba44eb50f9
Andreas Foteas <andreasfoteas@hotmail.com>
2026-01-22 19:31:17 +0200

    Time: 279 ms (6.34%), Space: 26.5 MB (81.3%) - LeetHub

---
+++ 3507-minimum-pair-removal-to-sort-array-i/3507-minimum-pair-removal-to-sort-array-i.cpp
@@ -0,0 +1,44 @@
+class Solution {
+public:
+    bool isNonDecreasing(vector<int>& nums) {
+        int len = nums.size();
+        for (int i = 1 ; i < len; ++i) {
+            if(nums[i] < nums[i-1]) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    void mergeOne(vector<int>& nums) {
+        int len = nums.size();
+        int ans = 0;
+        int minSum = std::numeric_limits<int>::max(); 
+        for (int i = 1 ; i < len; ++i) {
+            if((nums[i] + nums[i-1]) < minSum) {
+                minSum = (nums[i] + nums[i-1]);
+                ans = i;
+            }
+        }
+        nums[ans-1] = minSum;
+        // cout << "minSum:" << minSum << endl;
+        // cout << "ans:" << ans << endl;
+        nums.erase(nums.begin() + ans);
+
+    }
+
+    int minimumPairRemoval(vector<int>& nums) {
+        int ans = 0;
+        while (!isNonDecreasing(nums)) {
+            for(auto& num: nums) {
+                cout << num << ", ";
+            }
+            cout << endl;
+            mergeOne(nums);
+            cout << "merged!!!" << endl;
+            ++ans;
+        }
+        return ans;
+        
+    }
+};
