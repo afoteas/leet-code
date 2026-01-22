f3ccba73157417dc1f787c8b8793d2b5d25d11ab
Andreas Foteas <andreasfoteas@hotmail.com>
2026-01-22 19:56:23 +0200

    Time: 0 ms (100%), Space: 26.4 MB (95.94%) - LeetHub

--- 3507-minimum-pair-removal-to-sort-array-i/3507-minimum-pair-removal-to-sort-array-i.cpp
+++ 3507-minimum-pair-removal-to-sort-array-i/3507-minimum-pair-removal-to-sort-array-i.cpp
@@ -1,46 +1,44 @@
+// Performance Decrease: Lambda Captures Explained
+// The performance decrease is likely due to lambda capture overhead. Here's why:
+
+// The Problem
+// Each time you call these lambdas in a loop, there's overhead from:
+
+// Indirect access through the captured reference
+// Function call overhead (lambdas are function objects)
+// Potential compiler optimization limitations with captures
+// Why Commented Code Was Faster
+// The commented version was a direct member function that:
+
+// Has direct access to parameters (no capture)
+// May be inlined more easily by the compiler
+// Has less abstraction layers
+
 class Solution {
 public:
-    // void mergeOne(vector<int>& nums) {
-    //     int len = nums.size();
-    //     int ans = 0;
-    //     int minSum = std::numeric_limits<int>::max(); 
-    //     for (int i = 1 ; i < len; ++i) {
-    //         if((nums[i] + nums[i-1]) < minSum) {
-    //             minSum = (nums[i] + nums[i-1]);
-    //             ans = i;
-    //         }
-    //     }
-    //     nums[ans-1] = minSum;
-    //     nums.erase(nums.begin() + ans);
-
-    // }
-
     int minimumPairRemoval(vector<int>& nums) {
         int ans = 0;
         
-        auto isNonDecreasing = [&nums]() -> bool {
-            for (int i = 1 ; i < nums.size(); ++i) {
-                if(nums[i] < nums[i-1]) return false;
+        auto isNonDecreasing = [](const std::vector<int>& v) -> bool {
+            for (size_t i = 1; i < v.size(); ++i) {
+                if (v[i] < v[i - 1]) return false;
             }
             return true;
         };
 
-        auto mergeOne = [&nums] -> void {
-            short pos = 0;
+        auto mergeOne = [](std::vector<int>& v) -> void {
+            size_t pos = 0;
             int minSum = std::numeric_limits<int>::max();
-            for (short i = 1 ; i < nums.size(); ++i) {
-                if((nums[i] + nums[i-1]) < minSum) {
-                    minSum = (nums[i] + nums[i-1]);
-                    pos = i;
-                }
+            for (size_t i = 1; i < v.size(); ++i) {
+                int s = v[i] + v[i - 1];
+                if (s < minSum) { minSum = s; pos = i; }
             }
-            nums[pos-1] = minSum;
-            nums.erase(nums.begin() + pos);
+            v[pos - 1] = minSum;
+            v.erase(v.begin() + pos);
         };
 
-
-        while (!isNonDecreasing()) {
-            mergeOne();
+        while (!isNonDecreasing(nums)) {
+            mergeOne(nums);
             ++ans;
         }
         return ans;
