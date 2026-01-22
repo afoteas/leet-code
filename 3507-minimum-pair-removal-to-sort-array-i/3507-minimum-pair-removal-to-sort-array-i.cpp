bd749ad2b976d2597c22dccbca10e657856bb09c
Andreas Foteas <andreasfoteas@hotmail.com>
2026-01-22 19:43:49 +0200

    Time: 3 ms (40.33%), Space: 26.6 MB (61.14%) - LeetHub

--- 3507-minimum-pair-removal-to-sort-array-i/3507-minimum-pair-removal-to-sort-array-i.cpp
+++ 3507-minimum-pair-removal-to-sort-array-i/3507-minimum-pair-removal-to-sort-array-i.cpp
@@ -1,42 +1,46 @@
 class Solution {
 public:
-    // bool isNonDecreasing(vector<int>& nums) {
+    // void mergeOne(vector<int>& nums) {
     //     int len = nums.size();
+    //     int ans = 0;
+    //     int minSum = std::numeric_limits<int>::max(); 
     //     for (int i = 1 ; i < len; ++i) {
-    //         if(nums[i] < nums[i-1]) {
-    //             return false;
+    //         if((nums[i] + nums[i-1]) < minSum) {
+    //             minSum = (nums[i] + nums[i-1]);
+    //             ans = i;
     //         }
     //     }
-    //     return true;
+    //     nums[ans-1] = minSum;
+    //     nums.erase(nums.begin() + ans);
+
     // }
-
-    void mergeOne(vector<int>& nums) {
-        int len = nums.size();
-        int ans = 0;
-        int minSum = std::numeric_limits<int>::max(); 
-        for (int i = 1 ; i < len; ++i) {
-            if((nums[i] + nums[i-1]) < minSum) {
-                minSum = (nums[i] + nums[i-1]);
-                ans = i;
-            }
-        }
-        nums[ans-1] = minSum;
-        nums.erase(nums.begin() + ans);
-
-    }
 
     int minimumPairRemoval(vector<int>& nums) {
         int ans = 0;
-        auto isNonDecreasing = [&nums]() {
+        
+        auto isNonDecreasing = [&nums]() -> bool {
             for (int i = 1 ; i < nums.size(); ++i) {
                 if(nums[i] < nums[i-1]) return false;
             }
             return true;
         };
 
+        auto mergeOne = [&nums] -> void {
+            short pos = 0;
+            int minSum = std::numeric_limits<int>::max();
+            for (short i = 1 ; i < nums.size(); ++i) {
+                if((nums[i] + nums[i-1]) < minSum) {
+                    minSum = (nums[i] + nums[i-1]);
+                    pos = i;
+                }
+            }
+            nums[pos-1] = minSum;
+            nums.erase(nums.begin() + pos);
+        };
+
 
         while (!isNonDecreasing()) {
-            mergeOne(nums);
+            mergeOne();
             ++ans;
         }
         return ans;
