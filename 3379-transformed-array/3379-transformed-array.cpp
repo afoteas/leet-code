d854112e3645787a824ba941b5443f3154e19901
Andreas Foteas <andreasfoteas@hotmail.com>
2026-02-05 12:59:06 +0200

    Time: 8 ms (46.52%), Space: 25.7 MB (39.55%) - LeetHub

--- 3379-transformed-array/3379-transformed-array.cpp
+++ 3379-transformed-array/3379-transformed-array.cpp
@@ -3,11 +3,7 @@ public:
     vector<int> constructTransformedArray(vector<int>& nums) {
         int size = nums.size();
         vector<int> ans(size);
-
-        for(int i=0; i< size; ++i) {
-            int p = (size + nums.at(i)%size + i)%size;
-            ans.at(i) = nums.at(p);
-        }
+        for(int i=0; i< size; ++i) ans.at(i) = nums.at((size + nums.at(i)%size + i)%size);
         return ans;
     }
 };
