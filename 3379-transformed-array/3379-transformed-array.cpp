6ac02a604c17112c9a910e2e9272351253cdeb7c
Andreas Foteas <andreasfoteas@hotmail.com>
2026-02-05 12:57:06 +0200

    Time: 4 ms (79.67%), Space: 25.6 MB (70.75%) - LeetHub

--- 3379-transformed-array/3379-transformed-array.cpp
+++ 3379-transformed-array/3379-transformed-array.cpp
@@ -6,11 +6,11 @@ public:
 
         for(int i=0; i< size; ++i) {
             if (nums.at(i) > 0) {
-                int p = (i+nums.at(i))%size;
+                // int p = (i+nums.at(i))%size;
+                int p = (size + nums.at(i)%size + i)%size;
                 ans.at(i) = nums.at(p);
             } 
             else if(nums.at(i) < 0) {
-                // int p = (i + nums.at(i)%size) >= 0 ? (i + nums.at(i)%size) : (size + nums.at(i)%size + i)%size;
                 int p = (size + nums.at(i)%size + i)%size;
                 ans.at(i) = nums.at(p);
             } 
