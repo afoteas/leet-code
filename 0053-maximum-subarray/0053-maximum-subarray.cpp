477c69fbfaefe6a9fcbf75288763e8a28820460f
Andreas Foteas <andreasfoteas@hotmail.com>
2026-02-07 10:51:51 +0200

    Time: 0 ms (100%), Space: 74.7 MB (15.3%) - LeetHub

---
+++ 0053-maximum-subarray/0053-maximum-subarray.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+        int s = nums.size();
+        vector<int> dp(s);
+        dp[0] = nums[0];
+        for(int i = 1; i<s; ++i) {
+            dp[i] = max(nums[i], nums[i] + dp[i-1]);
+        }
+        return *max_element(dp.begin(),dp.end());
+    }
+};
