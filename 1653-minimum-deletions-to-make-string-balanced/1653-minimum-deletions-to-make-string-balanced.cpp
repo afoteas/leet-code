dfca89353a59c52fb4c9b35dd4867da40a29dace
Andreas Foteas <andreasfoteas@hotmail.com>
2026-02-07 11:41:25 +0200

    Time: 48 ms (23.22%), Space: 55.3 MB (10.5%) - LeetHub

---
+++ 1653-minimum-deletions-to-make-string-balanced/1653-minimum-deletions-to-make-string-balanced.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int minimumDeletions(string s) {
+        vector<int> a(s.size()+1);
+        vector<int> b(s.size()+1);
+        int ret = s.size();
+        for(int i=1; i<s.size(); ++i) {
+            b[i] = (s[i-1] == 'b') ? b[i-1]+1: b[i-1];
+        }
+        for(int i=s.size()-1; i>0; --i) {
+            a[i-1] = (s[i] == 'a') ? a[i]+1: a[i];
+        }
+        // cout<<"a: ";
+        // for(int i=0; i<a.size()-1; ++i) {
+        //     cout<< a[i] << " ";
+        // }
+        // cout<<"\nb: ";
+        // for(int i=0; i<a.size()-1; ++i) {
+        //     cout<< b[i] << " ";
+        // }
+        for(int i=0; i<s.size(); ++i) {
+            ret = min(ret, a[i]+b[i]);
+        }
+        
+        return ret;
+    }
+};
