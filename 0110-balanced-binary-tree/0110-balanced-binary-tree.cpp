9b707f45d1240ad6390761da9f58033be56b5778
Andreas Foteas <andreasfoteas@hotmail.com>
2026-02-09 02:47:50 +0200

    Time: 0 ms (100%), Space: 23.2 MB (20.59%) - LeetHub

--- 0110-balanced-binary-tree/0110-balanced-binary-tree.cpp
+++ 0110-balanced-binary-tree/0110-balanced-binary-tree.cpp
@@ -23,4 +23,29 @@ public:
         if(!root) return true;
         return dfs(root) != -1;
     }
+
+    bool isBalancedBFS_ERROR(TreeNode* root) {
+        if(!root) return true;
+        queue<TreeNode*>q;
+        q.push(root);
+        int l=1;
+        int u=0;
+        bool e = false;
+        while(!q.empty()) {
+            int s=q.size();
+            if (e) return false;
+            if (s!=l) {
+                if (s <= l>>1) e=true; 
+                if (++u > 2) return false;
+            }
+            for(int i=0; i<s; ++i) {
+                TreeNode* c=q.front();
+                q.pop();
+                if(c->left) q.push(c->left);
+                if(c->right) q.push(c->right);
+            }
+            l <<=1;
+        }
+        return true;
+    }
 };
