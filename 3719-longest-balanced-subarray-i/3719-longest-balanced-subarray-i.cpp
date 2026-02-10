e7267d02cdad81baad5c30738aad270a98351ebb
Andreas Foteas <andreasfoteas@hotmail.com>
2026-02-10 20:44:51 +0200

    Time: 643 ms (94.76%), Space: 242.6 MB (95.81%) - LeetHub

---
+++ 3719-longest-balanced-subarray-i/3719-longest-balanced-subarray-i.cpp
@@ -0,0 +1,58 @@
+class Solution {
+public:
+    int longestBalancedSlow(vector<int>& nums) {
+        int subLen = nums.size();
+        while(subLen > 1) {
+            for(int i=0; i<=nums.size()-subLen; ++i) {
+                set<int> odd;
+                set<int> even;
+                for(int j=i; j<(i+subLen); ++j) {
+                    if (nums[j]%2) odd.insert(nums[j]);
+                    else even.insert(nums[j]);
+                }
+                if (odd.size() == even.size() && odd.size()) return subLen;
+            }
+            --subLen;
+        }
+        return 0;
+    }
+
+    int longestBalanced(vector<int>& nums) {
+        int subLen = nums.size();
+        while(subLen > 1) {
+            unordered_map<int,int> odd;
+            unordered_map<int,int> even;
+            for(int i=0; i<=nums.size()-subLen; ++i) {
+                if(i == 0) {
+                    for(int j=i; j<(i+subLen); ++j) {
+                        if (nums[j]%2) {
+                            odd[nums[j]]++;
+                        }
+                        else {
+                            even[nums[j]]++;
+                        };
+                    }
+                } else {
+                    // add new element
+                    int in = nums[i+subLen-1];
+                    if (in % 2) {
+                        odd[in]++;
+                    } else {
+                        even[in]++;
+                    }
+
+                    // remove old element
+                    int out = nums[i-1];
+                    if (out % 2) {
+                        if (--odd[out] == 0) odd.erase(out);
+                    } else {
+                        if (--even[out] == 0) even.erase(out);
+                    }
+                }
+                if (odd.size() == even.size()) return subLen;
+            }
+            --subLen;
+        }
+        return 0;
+    }
+};
