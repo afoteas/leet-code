833ff597f56d35ae47ed74852490090358c822cc
Andreas Foteas <andreasfoteas@hotmail.com>
2026-02-12 01:58:39 +0200

    Time: 3 ms (9.52%), Space: 9.1 MB (6.4%) - LeetHub

--- 1025-divisor-game/1025-divisor-game.cpp
+++ 1025-divisor-game/1025-divisor-game.cpp
@@ -1,11 +1,10 @@
 class Solution {
 public:
     bool divisorGame(int n) {
-        int dp[n+1];
-        memset(dp,0,sizeof(dp));
+        vector<int> dp(n+1,0);
         for(int i=2;i<n+1;i++){
             for(int j=1;j<i;j++){
-                if(i%j==0)dp[i] = dp[i] | !dp[i-j];  //(!dp[i-j] checks if i-j is a losing position)
+                if(i%j==0)dp[i] = dp[i] | !dp[i-j];
             }
         }return dp[n];
         
