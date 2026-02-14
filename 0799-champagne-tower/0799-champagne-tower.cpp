a9fe15bbf1e0ff3b9e3fe42d909ad18642aedf4c
Andreas Foteas <andreasfoteas@hotmail.com>
2026-02-15 01:40:54 +0200

    Time: 10 ms (27.87%), Space: 14.1 MB (71.15%) - LeetHub

--- 0799-champagne-tower/0799-champagne-tower.cpp
+++ 0799-champagne-tower/0799-champagne-tower.cpp
@@ -1,22 +1,22 @@
 class Solution {
 public:
-    double champagneTowerBottomUp(int poured, int query_row, int query_glass) {
-        vector<vector<double>> tower(query_row+1);
+    double champagneTower(int poured, int query_row, int query_glass) {
+        vector<vector<double>> t(query_row+1);
         for(int i=0; i<=query_row; ++i) {
-            tower[i].resize(i+1,0.0);
+            t[i].resize(i+1,0.0);
         }
-        tower[0][0] = poured;
+        t[0][0] = poured;
 
         for(int i=0; i<query_row; ++i) {
             for(int j=0; j<=i; ++j) {
-                if(tower[i][j] > 1.0) {
-                    double overflow = tower[i][j] - 1.0;
-                    tower[i+1][j] += overflow/2.0;
-                    tower[i+1][j+1] += overflow/2.0;
+                if(t[i][j] > 1.0) {
+                    double overflow = t[i][j] - 1.0;
+                    t[i+1][j] += overflow/2.0;
+                    t[i+1][j+1] += overflow/2.0;
                 }
             }
         }
-        return min(1.0, tower[query_row][query_glass]);
+        return min(1.0, t[query_row][query_glass]);
     }
 
 
@@ -45,7 +45,7 @@ public:
         return t[row][glass] = left + right;
     }
     
-    double champagneTower(int poured, int query_row, int query_glass) {
+    double champagneTowerTopDown(int poured, int query_row, int query_glass) {
         // t.assign(query_row + 1, vector<double>(query_row + 1, -1.0));
         memset(t, -1, sizeof(t));
         return min(1.00, solve(poured, query_row, query_glass));
