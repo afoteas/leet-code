40a95c261ef90b9d6b024f6b8a054b0404b9d182
Andreas Foteas <andreasfoteas@hotmail.com>
2026-02-15 01:30:29 +0200

    Time: 4 ms (66.26%), Space: 17.9 MB (44.01%) - LeetHub

--- 0799-champagne-tower/0799-champagne-tower.cpp
+++ 0799-champagne-tower/0799-champagne-tower.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    double champagneTower(int poured, int query_row, int query_glass) {
+    double champagneTowerBottomUp(int poured, int query_row, int query_glass) {
         vector<vector<double>> tower(query_row+1);
         for(int i=0; i<=query_row; ++i) {
             tower[i].resize(i+1,0.0);
@@ -19,4 +19,33 @@ public:
         return min(1.0, tower[query_row][query_glass]);
     }
 
+
+    vector<vector<double>> t;
+    
+    double solve(int poured, int row, int glass) {
+        if(row < 0 || glass > row || glass < 0) {
+            return 0.00;
+        }
+        
+        if(row == 0 && glass == 0) {
+            return poured;
+        }
+        
+        if(t[row][glass] > -1) {
+            return t[row][glass];
+        }
+        
+        double left = (solve(poured, row - 1, glass - 1) - 1) / 2;
+        if(left < 0) left = 0;
+        
+        double right = (solve(poured, row - 1, glass) - 1) / 2;
+        if(right < 0) right = 0;
+        
+        return t[row][glass] = left + right;
+    }
+    
+    double champagneTower(int poured, int query_row, int query_glass) {
+        t.assign(query_row + 1, vector<double>(query_row + 1, -1.0));
+        return min(1.00, solve(poured, query_row, query_glass));
+    }
 };
