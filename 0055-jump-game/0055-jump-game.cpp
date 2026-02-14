f2d71dc72efd1f2f94eb736aaf5935382f0d10e2
Andreas Foteas <andreasfoteas@hotmail.com>
2026-02-14 22:17:02 +0200

    Time: 0 ms (100%), Space: 52.3 MB (74.13%) - LeetHub

--- 0055-jump-game/0055-jump-game.cpp
+++ 0055-jump-game/0055-jump-game.cpp
@@ -1,11 +1,20 @@
 class Solution {
 public:
-    bool canJump(vector<int>& nums) {
+    bool canJump2(vector<int>& nums) {
         int gas {0};
         for(const auto& num: nums) {
             if (gas < 0) return false;
             if (num > gas) gas = num;
             gas--;
+        }
+        return true;
+    }
+
+    bool canJump(vector<int>& nums) {
+        int maxIndex {0};
+        for(int i=0; i < nums.size(); ++i) {
+            if (i > maxIndex) return false;
+            maxIndex=max(maxIndex, i + nums[i]);
         }
         return true;
     }
