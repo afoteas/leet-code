2a0b7f92529a7841eb2d890c3e6eaaec74a8d045
Andreas Foteas <andreasfoteas@hotmail.com>
2026-02-16 12:49:38 +0200

    Time: 92 ms (18.78%), Space: 14 MB (67.51%) - LeetHub

---
+++ 0743-network-delay-time/0743-network-delay-time.c
@@ -0,0 +1,54 @@
+int networkDelayTime(int** times, int timesSize, int* timesColSize, int n, int k){
+    // Initialize distance array
+    int* dist = (int*)malloc((n + 1) * sizeof(int));
+    bool* visited = (bool*)malloc((n + 1) * sizeof(bool));
+    
+    for (int i = 1; i <= n; i++) {
+        dist[i] = INT_MAX;
+        visited[i] = false;
+    }
+    dist[k] = 0;
+    
+    // Process all nodes
+    for (int i = 0; i < n; i++) {
+        // Find unvisited node with minimum distance
+        int u = -1;
+        for (int j = 1; j <= n; j++) {
+            if (!visited[j] && (u == -1 || dist[j] < dist[u])) {
+                u = j;
+            }
+        }
+        
+        if (u == -1 || dist[u] == INT_MAX) break;
+        visited[u] = true;
+        
+        // Relax all edges from u
+        for (int j = 0; j < timesSize; j++) {
+            int from = times[j][0];
+            int to = times[j][1];
+            int weight = times[j][2];
+            
+            // If edge starts from u and relaxation is possible
+            if (from == u && dist[u] + weight < dist[to]) {
+                dist[to] = dist[u] + weight;
+            }
+        }
+    }
+    
+    // Find maximum distance (when all nodes are reached)
+    int maxDist = 0;
+    for (int i = 1; i <= n; i++) {
+        if (dist[i] == INT_MAX) {
+            free(dist);
+            free(visited);
+            return -1;  // Some node unreachable
+        }
+        if (dist[i] > maxDist) {
+            maxDist = dist[i];
+        }
+    }
+    
+    free(dist);
+    free(visited);
+    return maxDist;
+}
