6da3b37ff6bbdbc57cc4cdbe2ca93ffb120c22c1
Andreas Foteas <andreasfoteas@hotmail.com>
2026-02-18 01:50:42 +0200

    Time: 0 ms (100%), Space: 9.4 MB (95.73%) - LeetHub

---
+++ 0401-binary-watch/0401-binary-watch.cpp
@@ -0,0 +1,47 @@
+class Solution {
+public:
+
+    void combineHelper(int start, int k, vector<int>& nums, vector<int>& path, 
+                     vector<string>& result) {
+        // Base case: selected k elements
+        if (path.size() == k) {
+            int minutes = 0;
+            int hours = 0;
+            for(int p: path) {
+                if (p<64) minutes += p;
+                else hours+=(p>>6);
+
+            }
+            if (hours < 12 && minutes < 60) {
+                char buffer[6];
+                sprintf(buffer, "%d:%02d", hours, minutes);
+                result.push_back(string(buffer));
+            }
+            return;
+        }
+        for (int i = start; i < nums.size(); i++) {
+                path.push_back(nums[i]);
+                combineHelper(i+1, k, nums, path, result);
+                path.pop_back();
+
+        }
+    }
+
+
+    vector<string> combine(int count) {
+        vector<string> result;
+        vector<int> nums = {1, 2, 4, 8, 16, 32, 64, 128, 256, 512};
+        vector<int> path;
+        
+        combineHelper(0, count, nums, path, result);
+        
+        return result;
+    }
+
+    vector<string> readBinaryWatch(int turnedOn) {
+        if (turnedOn == 0) return {"0:00"};
+        if (turnedOn > 8) return {};
+        return combine(turnedOn);
+        
+    }
+};
