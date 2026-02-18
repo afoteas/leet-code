56a4d6e4e52a916c7ae43c9970a8e653005d7222
Andreas Foteas <andreasfoteas@hotmail.com>
2026-02-18 21:07:06 +0200

    Time: 143 ms (2.08%), Space: 16.4 MB (6.23%) - LeetHub

---
+++ 0082-remove-duplicates-from-sorted-list-ii/0082-remove-duplicates-from-sorted-list-ii.cpp
@@ -0,0 +1,63 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* deleteDuplicates(ListNode* head) {
+        if(!head) return nullptr;
+        unordered_map<int,int> seen;
+        ListNode* ans = nullptr;
+        ListNode* last = nullptr;
+
+        seen[head->val]++;
+        while(head) {
+            ListNode* cur = head;
+            head = head->next;
+            if(head) {
+                seen[head->val]++;
+            }
+            else {
+                // last->next = cur;
+                if(ans) {
+                    if(seen[cur->val] == 1) last->next = cur;
+                }
+                else {
+                    if(seen[cur->val] == 1) ans = cur;
+                }
+                break;
+            }
+            cout<< "seen["<<cur->val<<"]:"<<seen[cur->val]<<endl;
+
+            if(!ans) {
+                if (seen[cur->val]==1) {
+                    cout<<"entered1"<<endl;
+                    ans = cur;
+                    last = cur;
+                    last->next = nullptr;
+                }
+            } else {
+                if (seen[cur->val]==1) {
+                    cout<<"entered2"<<endl;
+                    last->next=cur;
+                    last = last->next;
+                    last->next = nullptr;
+                } else {
+                    cout<<"entered3"<<endl;
+                }
+            }
+
+        }
+        cout << "seen:"<<endl;
+        for(auto&s:seen) {
+            cout<<s.first<<":"<<s.second<<endl;
+        }
+        return ans;
+    }
+};
