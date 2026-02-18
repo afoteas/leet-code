e36ac18721f3f158b9fc19daee369b4221cb7b60
Andreas Foteas <andreasfoteas@hotmail.com>
2026-02-18 21:43:21 +0200

    Time: 184 ms (2.07%), Space: 15.8 MB (14.57%) - LeetHub

--- 0082-remove-duplicates-from-sorted-list-ii/0082-remove-duplicates-from-sorted-list-ii.cpp
+++ 0082-remove-duplicates-from-sorted-list-ii/0082-remove-duplicates-from-sorted-list-ii.cpp
@@ -12,10 +12,67 @@ class Solution {
 public:
     ListNode* deleteDuplicates(ListNode* head) {
         if(!head) return nullptr;
+        ListNode* ans = nullptr;
+        ListNode* last = nullptr;
+        bool can_add = true;
+        int last_val = head->val;
+        while(head) {
+            ListNode* cur = head;
+            head = head->next;
+            cout<<"last_val1:"<<last_val<<"can_add:"<<can_add<<endl;
+            if(head) {
+                if(can_add) {
+                    if(last_val != head->val) {
+                        // last_val = head->val;
+                    } else {
+                        cout<<"last_val:"<<last_val<<",head->val:"<<head->val<<endl;
+                        can_add = false;
+                        // last_val = head->val;
+                    }
+                }
+            }
+            else {
+                if(ans) {
+                    if(can_add) last->next = cur;
+                }
+                else {
+                    if(can_add) ans = cur;
+                }
+                break;
+            }
+            if(!ans) {
+                if (can_add) {
+                    ans = cur;
+                    last = cur;
+                    last->next = nullptr;
+                }
+            } else {
+                if (can_add) {
+                    last->next=cur;
+                    last = last->next;
+                    last->next = nullptr;
+                } 
+            }
+            if(last_val != head->val) {
+                cout<<"if:last_val:"<<last_val<<endl;
+                cout<<"can_add:"<<can_add<<endl;
+                can_add = true;
+            } else {
+                cout<<"else:last_val:"<<last_val<<endl;
+                cout<<"head->val:"<<head->val<<endl;
+                can_add = false;
+            }
+            last_val = head->val;
+
+        }
+        return ans;
+    }
+
+    ListNode* deleteDuplicatesFirst(ListNode* head) {
+        if(!head) return nullptr;
         unordered_map<int,int> seen;
         ListNode* ans = nullptr;
         ListNode* last = nullptr;
-
         seen[head->val]++;
         while(head) {
             ListNode* cur = head;
