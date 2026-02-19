c22fa74048b50adc153e1d8edfac818bbe01a58e
Andreas Foteas <andreasfoteas@hotmail.com>
2026-02-19 23:43:56 +0200

    Time: 0 ms (100%), Space: 12.1 MB (72.32%) - LeetHub

--- 0557-reverse-words-in-a-string-iii/0557-reverse-words-in-a-string-iii.cpp
+++ 0557-reverse-words-in-a-string-iii/0557-reverse-words-in-a-string-iii.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    string reverseWords(string s) {
+    string reverseWordsMine(string s) {
         int begin=-1, end=-1;
         for(int i = 0; i< s.size(); ++i) {
             if(s[i]==' ') {
@@ -21,6 +21,17 @@ public:
         }
         while (begin < end) {
             swap(s[begin++],s[end--]);
+        }
+        return s;
+    }
+
+    string reverseWords(string s) {
+        size_t start = 0;
+        for (size_t i = 0; i <= s.size(); ++i) {
+            if (i == s.size() || s[i] == ' ') {
+                std::reverse(s.begin() + start, s.begin() + i);
+                start = i + 1;
+            }
         }
         return s;
     }
