a3e3a91cd205b88d43ee5bc271a75e471c07906e
Andreas Foteas <andreasfoteas@hotmail.com>
2026-03-09 11:55:07 +0200

    Time: 0 ms (100%), Space: 14 MB (99.87%) - LeetHub

--- 1356-sort-integers-by-the-number-of-1-bits/1356-sort-integers-by-the-number-of-1-bits.cpp
+++ 1356-sort-integers-by-the-number-of-1-bits/1356-sort-integers-by-the-number-of-1-bits.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    vector<int> sortByBits(vector<int>& arr) {
+    vector<int> sortByBitsMine(vector<int>& arr) {
         vector<int> ret;
         map<int,vector<int>> mapa;
         for(auto&a:arr) {
@@ -14,5 +14,35 @@ public:
             }
         }
         return ret;
+    }
+
+    vector<int> sortByBitsMedium(vector<int>& arr) {
+        vector<pair<int,int>> p;
+
+        for( int n : arr ){
+            int count = __builtin_popcount(n);
+            p.emplace_back(count, n);
+        }
+
+        vector<int>  ans;
+        sort(p.begin(), p.end());
+        for( pair<int,int> &pr : p ){
+            ans.push_back(pr.second);
+        }
+        return ans;
+    }
+
+    vector<int> sortByBits(vector<int>& arr) {
+        sort(arr.begin() , arr.end() , [](const int &a ,const int &b) {
+            int count_a = __builtin_popcount(a);
+            int count_b = __builtin_popcount(b);
+            if (count_a != count_b) {
+                return count_a < count_b; 
+            }
+            else{
+                return a < b;
+            }
+        });
+        return arr;
     }
 };
