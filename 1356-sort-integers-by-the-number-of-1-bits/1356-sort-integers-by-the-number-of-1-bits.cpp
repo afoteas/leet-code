1ceaff4219bcae4bb4411626dd1e3bef0ae1ccf8
Andreas Foteas <andreasfoteas@hotmail.com>
2026-03-09 11:41:57 +0200

    Time: 2 ms (49.85%), Space: 14.6 MB (32.53%) - LeetHub

---
+++ 1356-sort-integers-by-the-number-of-1-bits/1356-sort-integers-by-the-number-of-1-bits.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<int> sortByBits(vector<int>& arr) {
+        vector<int> ret;
+        map<int,vector<int>> mapa;
+        for(auto&a:arr) {
+            bitset<32> b(a);
+            mapa[b.count()].push_back(a);
+        }
+        for(auto&[count,list]:mapa) {
+            sort(list.begin(),list.end());
+            for(auto& l:list) {
+                ret.push_back(l);
+            }
+        }
+        return ret;
+    }
+};
