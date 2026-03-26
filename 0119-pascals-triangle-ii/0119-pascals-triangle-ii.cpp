ec90e72d458a56755f1c837be6e0852fec9a75b8
Andreas Foteas <andreasfoteas@hotmail.com>
2026-03-26 17:09:15 +0200

    Time: 0 ms (100%), Space: 9.4 MB (6.57%) - LeetHub

---
+++ 0119-pascals-triangle-ii/0119-pascals-triangle-ii.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    vector<int> getRow(int rowIndex) {
+        if (rowIndex == 0) return {1};
+        if (rowIndex == 1) return {1,1};
+        vector<int> pre = getRow(rowIndex - 1);
+        vector<int> ret(rowIndex+1, 1);
+        for(int i = 1; i < rowIndex; i++) {
+            ret[i] = pre[i-1] + pre[i];
+        } 
+        return ret;
+        
+        
+    }
+};
