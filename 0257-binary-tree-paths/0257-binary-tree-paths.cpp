4f71c38a8cb3f3409f3d5241ce32ce0cf6e50a7a
Andreas Foteas <andreasfoteas@hotmail.com>
2026-03-27 13:44:27 +0200

    Time: 1 ms (44.54%), Space: 19.2 MB (5.94%) - LeetHub

--- 0257-binary-tree-paths/0257-binary-tree-paths.cpp
+++ 0257-binary-tree-paths/0257-binary-tree-paths.cpp
@@ -12,12 +12,12 @@
 class Solution {
 public:
 
-    void dfs(TreeNode*root, vector<int> parents, vector<string>&paths) {
+    void dfs1(TreeNode*root, vector<int> parents, vector<string>&paths) {
         parents.push_back(root->val);
         if (root->left || root->right)
         {
-            if (root->left) dfs(root->left, parents, paths);
-            if (root->right) dfs(root->right, parents, paths);
+            if (root->left) dfs1(root->left, parents, paths);
+            if (root->right) dfs1(root->right, parents, paths);
         }
         else
         {
@@ -27,12 +27,25 @@ public:
             }
             paths.push_back(path);
         }
+    }
+
+    void dfs2(TreeNode*root, string parents, vector<string>&paths) {
+        if (root->left || root->right)
+        {
+            if (root->left) dfs2(root->left, format("{}->{}",parents,root->left->val), paths);
+            if (root->right) dfs2(root->right, format("{}->{}",parents,root->right->val), paths);
+        }
+        else
+        {
+            paths.push_back(parents);
+        }
 
     }
 
     vector<string> binaryTreePaths(TreeNode* root) {
         vector<string> paths = {};
-        dfs(root,{},paths);
+        // dfs1(root,{},paths);
+        dfs2(root,to_string(root->val),paths);
         return paths;
         
     }
