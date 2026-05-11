c379bbc0e3db38effb8bae4513c1c50932f16399
Andreas Foteas <andreasfoteas@hotmail.com>
2026-05-11 15:05:50 +0300

    Time: 4 ms (86.31%), Space: 46.9 MB (22.92%) - LeetHub

--- 2553-separate-the-digits-in-an-array/2553-separate-the-digits-in-an-array.java
+++ 2553-separate-the-digits-in-an-array/2553-separate-the-digits-in-an-array.java
@@ -1,5 +1,5 @@
 class Solution {
-    public int[] separateDigits(int[] nums) {
+    public int[] separateDigitslong(int[] nums) {
         int totalDigits = 0;
         for (int num : nums) {
             if (num == 0) {
@@ -43,4 +43,45 @@ class Solution {
 
         return ans;
     }
+
+    public int[] separateDigits(int[] nums) {
+        List<Integer> list = new ArrayList<>();
+        for (int num : nums) {
+            if (num < 10) {
+                list.add(num);
+                continue;
+            }
+
+            int tmp = 1;
+            while (tmp <= num) tmp *= 10;
+
+            while (tmp > 1) {
+                tmp /= 10;
+                list.add((num / tmp) % 10);
+            }
+        }
+
+        int[] ret = new int[list.size()];
+        for (int i = 0; i < list.size(); i++) {
+            ret[i] = list.get(i);
+        }
+        return ret;
+    }
 }
+
+    // vector<int> separateDigits(vector<int>& nums) {
+    //     vector<int> ret;
+    //     for(auto& num: nums) {
+    //         if (num == num%10) {
+    //             ret.push_back(num);
+    //             continue;
+    //         }
+    //         int tmp=1;
+    //         while(tmp <= num) tmp*=10;
+    //         while(tmp>1) {
+    //             tmp/=10;
+    //             ret.push_back((num/tmp)%10);
+    //         }
+    //     }
+    //     return ret;
+    // }
