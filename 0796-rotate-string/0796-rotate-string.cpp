a5f342cb22468e470f27b2314ab791e592cd38ca
Andreas Foteas <andreasfoteas@hotmail.com>
2026-05-18 12:16:19 +0300

    Time: 0 ms (100%), Space: 8 MB (82.82%) - LeetHub

---
+++ 0796-rotate-string/0796-rotate-string.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    bool rotateString(string s, string goal) {
+        if (s.size() != goal.size()) return false;
+        if (s == goal) return true;
+        for(int i = 0; i < s.size(); i++) {
+            char c = s[0];
+            s.erase(0, 1);
+            s.push_back(c);
+            if (s == goal) return true;
+        }
+        return false;
+    }
+};
