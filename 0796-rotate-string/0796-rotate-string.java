0194544cc24991f283341bc64b10f214be1e84fe
Andreas Foteas <andreasfoteas@hotmail.com>
2026-05-18 12:33:38 +0300

    Time: 3 ms (6.85%), Space: 43.5 MB (6.5%) - LeetHub

---
+++ 0796-rotate-string/0796-rotate-string.java
@@ -0,0 +1,17 @@
+class Solution {
+    public boolean rotateString(String s, String goal) {
+        if (s.length() != goal.length()) return false;
+        if (s.equals(goal)) return true;
+        String curr = s;
+        for(int i = 0; i < s.length(); i++) {
+            curr = curr.substring(1) + curr.charAt(0);
+            if (curr.equals(goal)) return true;
+        }
+        return false;
+    }
+
+    public boolean rotateStringCompact(String s, String goal) {
+        if (s.length() != goal.length()) return false;
+        return (s + s).contains(goal);
+    }
+}
