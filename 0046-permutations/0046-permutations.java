97ee42227124f2a79e4a6c7fce204112eeed277e
Andreas Foteas <andreasfoteas@hotmail.com>
2026-06-30 12:23:19 +0300

    Time: 5 ms (2.51%), Space: 46.3 MB (5.93%) - LeetHub

--- 0046-permutations/0046-permutations.java
+++ 0046-permutations/0046-permutations.java
@@ -1,22 +1,44 @@
 class Solution {
-    public List<List<Integer>> permute(int[] nums) {
+    public List<List<Integer>> permuteSlow(int[] nums) {
         List<Integer> numList = new ArrayList<>(Arrays.stream(nums).boxed().toList());
         List<List<Integer>> results = new ArrayList<>();
-        backtrack(numList, 0, results);
-        return results;
-
-        
+        backtrackSlow(numList, 0, results);
+        return results; 
     }
 
-    private void backtrack(List<Integer> numList, int start, List<List<Integer>> result) {
+    public List<List<Integer>> permute(int[] nums) {
+        List<List<Integer>> results = new ArrayList<>();
+        backtrack(nums, 0, results);
+        return results; 
+    }
+
+    private void backtrackSlow(List<Integer> numList, int start, List<List<Integer>> result) {
         if (start == numList.size()) {
             result.add(new ArrayList(numList));
             return;
         }
         for(int i = start; i < numList.size(); i++) {
             Collections.swap(numList, start, i);
-            backtrack(numList, start+1, result);
+            backtrackSlow(numList, start+1, result);
             Collections.swap(numList, i, start);
         }
+    }
+
+    private void backtrack(int[] nums, int start, List<List<Integer>> result) {
+        if (start == nums.length) {
+            result.add(Arrays.stream(nums).boxed().toList());
+            return;
+        }
+        for(int i = start; i < nums.length; i++) {
+            swap(nums, start, i);
+            backtrack(nums, start+1, result);
+            swap(nums, i, start);
+        }
+    }
+
+    private void swap(int[] arr, int i, int j) {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
     }
 }
