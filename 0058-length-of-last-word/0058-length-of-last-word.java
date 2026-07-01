514bcf9ab4c645d08e48b4518ceca70a95b4512b
Andreas Foteas <andreasfoteas@hotmail.com>
2026-07-01 13:13:57 +0300

    Time: 0 ms (100%), Space: 43.2 MB (17.46%) - LeetHub

--- 0058-length-of-last-word/0058-length-of-last-word.java
+++ 0058-length-of-last-word/0058-length-of-last-word.java
@@ -1,6 +1,24 @@
 class Solution {
-    public int lengthOfLastWord(String s) {
+    public int lengthOfLastWordOld(String s) {
         String[] ret = s.split(" ");
         return ret[ret.length-1].length();
+    }
+
+    public int lengthOfLastWord(String s) {
+        boolean seenChar = false;
+        int ret = 0;
+        for(int i = s.length()-1; i>=0; i--){
+            if(!seenChar) {
+                if(s.charAt(i) == ' ') continue;
+                else {
+                    seenChar = true;
+                    ret++;
+                }
+            } else {
+                if(s.charAt(i) == ' ') return ret;
+                else ret++;
+            }
+        }
+        return ret;
     }
 }
