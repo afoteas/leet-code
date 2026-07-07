5586a325cdf7f91f51268e18f0d627c615be0bb7
Andreas Foteas <andreasfoteas@hotmail.com>
2026-07-07 17:04:07 +0300

    Time: 0 ms (100%), Space: 19.3 MB (14.68%) - LeetHub

--- 3754-concatenate-non-zero-digits-and-multiply-by-sum-i/3754-concatenate-non-zero-digits-and-multiply-by-sum-i.py
+++ 3754-concatenate-non-zero-digits-and-multiply-by-sum-i/3754-concatenate-non-zero-digits-and-multiply-by-sum-i.py
@@ -3,11 +3,12 @@ class Solution:
         x = 0
         s = 0
         i = 0
-        while n>0: 
-            if n%10 >0:
-                x+= (n%10)*(10**i)
+        while n>0:
+            m = n%10
+            if m > 0:
+                x+= (m)*(10**i)
                 i+=1
-                s+= n%10
+                s+= m
             n//=10
         return x*s
 
