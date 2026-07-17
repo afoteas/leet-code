6607125d4829bdc1840e73711019a9aba14349e6
Andreas Foteas <andreasfoteas@hotmail.com>
2026-07-17 13:01:55 +0300

    Time: 0 ms (100%), Space: 8.6 MB (45.78%) - LeetHub

---
+++ 3658-gcd-of-odd-and-even-sums/3658-gcd-of-odd-and-even-sums.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int gcdOfOddEvenSums(int n) {
+        // int odd=0;
+        // int even=0;
+        // for(int i=1; i<= 2*n; i++) {
+        //     if(i&1) {
+        //         odd+=i;
+        //     } else {
+        //         even += i;
+        //     }
+        // }
+        return n;
+    }
+};
